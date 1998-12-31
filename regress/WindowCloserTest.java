ab8f1ce0fbc648654d8384fa303fdd037d626354
Ian Darwin <ian@darwinsys.com>
1998-12-31 22:10:14 +0000

    Hopefully-final pass for E.2 revision: all examples cited directly in course notes compile on JDK1.2 without deprecation warnings, and work for simple cases. Finally got rid of ExecDemoLs from notes, using ExecDemoSort instead. Added some missing HTM files, new WinClose (complete file, replacing "Foo.java" in CN), and clean.bat.

---
+++ regress/WindowCloserTest.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+import java.awt.event.*;
+
+/* Show an example of closing a Window.
+ * @author Ian Darwin
+ * @version $Id$
+ */
+public class WinClose extends Frame {
+	/* Constructor */
+	public WinClose() {
+		setSize(200, 100);
+        addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				// Fancier: popup a "confirm quit" dialog here.
+				setVisible(false);
+				dispose();
+				System.exit(0);
+			}
+		});
+	}
+
+	/* Main method */
+	public static void main(String argv[]) {
+		new WinClose().setVisible(true);
+	}
+}
