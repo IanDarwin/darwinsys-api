0ccb01d3c868d29ac8e937208efeafd655a51f53
Ian Darwin <ian@darwinsys.com>
1999-03-30 00:03:14 +0000

    Updates from a week on the road.

--- src/com/darwinsys/swingui/Clock.java
+++ src/com/darwinsys/swingui/Clock.java
@@ -1,35 +1,53 @@
-import java.applet.*;
 import java.awt.*;
-import java.util.Date;
+import javax.swing.*;
+import java.text.*;
+import java.util.*;
 
-public class Clock extends Applet implements Runnable {
-	Thread timerThread;
+/** A simple Clock */
+public class Clock extends javax.swing.JComponent {
+	protected DecimalFormat tflz, tf;
+	protected boolean done = false;
 
-	/* Applet Lifestyle Methods */
-	public void start() {
-		timerThread = new Thread(this, "Clock");
-		timerThread.start();
+	public Clock() {
+		new Thread(new Runnable() {
+			public void run() {
+				while (!done) {
+					Clock.this.repaint();	// request a redraw
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e){ /* do nothing*/ }
+				}
+			}
+		}).start();
+		tf = new DecimalFormat("#0");
+		tflz = new DecimalFormat("00");
 	}
+
 	public void stop() {
-		if (timerThread == null)
-			return;
-		timerThread.stop();
-		timerThread = null;
+		done = true;
 	}
  
-	/* Runnable interface method */
-	public void run() {
-		while (timerThread != null) {
-			repaint();	// request a redraw
-			try {
-				timerThread.sleep(1000);
-			} catch (InterruptedException e){ /* do nothing*/ }
-		}
+	/* paint() - get current time and draw (centered) in Component. */ 
+	public void paint(Graphics g) {
+		Calendar myCal = Calendar.getInstance();
+		StringBuffer sb = new StringBuffer();
+		sb.append(tf.format(myCal.get(Calendar.HOUR)));
+		sb.append(':');
+		sb.append(tflz.format(myCal.get(Calendar.MINUTE)));
+		sb.append(':');
+		sb.append(tflz.format(myCal.get(Calendar.SECOND)));
+		String s = sb.toString();
+		FontMetrics fm = getFontMetrics(getFont());
+		int x = (getSize().width - fm.stringWidth(s))/2;
+		// System.out.println("Size is " + getSize());
+		g.drawString(s, x, 10);
 	}
 
-	/* AWT method */ 
-	public void paint(Graphics g) {
-		Date d = new Date();
-		g.drawString(d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds(), 1, 10);
+	public Dimension getPreferredSize() {
+		return new Dimension(100, 30);
+	}
+
+	public Dimension getMinimumSize() {
+		return new Dimension(50, 10);
 	}
 }
