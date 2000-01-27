4e41e4c38ddb9aefcd5ba335a4448870ab83c330
Ian Darwin <ian@darwinsys.com>
2000-01-27 03:39:56 +0000

    More stuff.

---
+++ src/com/darwinsys/swingui/UtilGUI.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+
+/** Utilities for GUI work.
+ * @version $Id$
+ */
+public class UtilGUI {
+	/** Centre a Window, Frame, JFrame, Dialog, etc. */
+	public static void centre(Window w) {
+		// After packing a Frame or Dialog, centre it on the screen.
+		Dimension us = w.getSize(), 
+			them = Toolkit.getDefaultToolkit().getScreenSize();
+		int newX = (them.width - us.width) / 2;
+		int newY = (them.height- us.height)/ 2;
+		w.setLocation(newX, newY);
+	}
+	/** Center a Window, Frame, JFrame, Dialog, etc., 
+	 * but do it the American Spelling Way :-)
+	 */
+	public static void center(Window w) {
+		UtilGUI.centre(w);
+	}
+}
