e4b6030cf3c3569355c7fc2da3120d086e1b829b
Ian Darwin <ian@darwinsys.com>
2000-06-12 18:42:42 +0000

    Initial revision

---
+++ src/com/darwinsys/swingui/LabelText.java
@@ -0,0 +1,82 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/** A label and text combination, inspired by
+ * the LabelText control in Guy Eddon's ActiveX Components book
+ * (2nd Edition, p. 203). But done honestly, without using a GUI builder
+ * just to create a (captiveX) project and then typing 100 lines of code.
+ * @author	Ian Darwin, ian@darwinsys.com
+ * @version $Id$
+ */
+public class LabelText extends JComponent implements java.io.Serializable {
+	/** The label component */
+	protected JLabel theLabel;
+	/** The label component */
+	protected JTextField theTextField;
+
+	/** Construct the object including its GUI */
+	public LabelText(String label) {
+		super();
+		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+		theLabel = new JLabel(label);
+		add(theLabel);
+		theTextField = new JTextField(10);
+		add(theTextField);
+	}
+
+	/** Get the label's horizontal alignment */
+	public int getLabelAlignment() {
+		return theLabel.getHorizontalAlignment();
+	}
+
+	/** Set the label's horizontal alignment */
+	public void setLabelAlignment(int align) {
+		switch (align) {
+		case JLabel.LEFT:
+		case JLabel.CENTER:
+		case JLabel.RIGHT:
+			theLabel.setHorizontalAlignment(align);
+			break;
+		default:
+			throw new IllegalArgumentException(
+				"setLabelAlignment argument must be one of JLabel aligners");
+		}
+	}
+
+	/** Get the text displayed in the text field */
+	public String getText() {
+		return theTextField.getText();
+	}
+
+	/** Set the text displayed in the text field */
+	public void setText(String text) {
+		theTextField.setText(text);
+	}
+
+	/** Get the text displayed in the label */
+	public String getLabel() {
+		return theLabel.getText();
+	}
+
+	/** Set the text displayed in the label */
+	public void setLabel(String text) {
+		theLabel.setText(text);
+	}
+
+	/** Set the font used in both subcomponents. */
+	public void setFont(Font f) {
+		theLabel.setFont(f);
+		theTextField.setFont(f);
+	}
+
+	/** Adds the ActionListener to receive action events from the textfield */
+	public void addActionListener(ActionListener l) {
+		theTextField.addActionListener(l);
+	}
+
+	/** Remove an ActionListener from the textfield. */
+	public void removeActionListener(ActionListener l) {
+		theTextField.removeActionListener(l);
+	}
+}
---
+++ src/com/darwinsys/util/LabelTextTest.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+
+/** Simple test for LabelText.
+ * @version $Id$
+ */
+public class LabelTextTest {
+	/** "main program" for testing - construct and show */
+	public static void main(String av[]) {
+		// create a LabelText object, tell it to show up
+		final JFrame f = new JFrame("LabelText");
+		Container cp = f.getContentPane();
+		cp.setLayout(new BoxLayout(cp, BoxLayout.Y_AXIS));
+
+		LabelText comp;
+		comp = new LabelText("Test");
+		comp.setLabelAlignment(JLabel.LEFT);
+		comp.setText("Hello world");
+		cp.add(comp);
+		comp = new LabelText("Test 2");
+		comp.setLabelAlignment(JLabel.CENTER);
+		cp.add(comp);
+		comp = new LabelText("Test 3");
+		comp.setLabelAlignment(JLabel.RIGHT);
+		comp.setText("Entry area");
+		cp.add(comp);
+
+		f.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				f.setVisible(false);
+				f.dispose();
+				System.exit(0);
+			}
+		});
+		f.pack();
+		f.setLocation(200, 200);
+		f.setVisible(true);
+	}
+}
