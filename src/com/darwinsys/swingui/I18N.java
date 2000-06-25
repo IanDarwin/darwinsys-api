c682f549667e8ee636cb5ea7c376b08960937ebe
Ian Darwin <ian@darwinsys.com>
2000-06-25 01:15:00 +0000

    New Internationalization section.

---
+++ src/com/darwinsys/swingui/I18N.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import javax.swing.*;
+
+/** Set of convenience routines for internationalized code.
+ * All convenience methods are static, for ease of use.
+ * @version $Id$
+ */
+public class I18N {
+
+	/** Convenience routine to make a JButton */
+	public static JButton mkButton(ResourceBundle b, String name) {
+		String label;
+		try { label = b.getString(name+".label"); }
+		catch (MissingResourceException e) { label=name; }
+		return new JButton(label);
+	}
+
+	/** Convenience routine to make a JMenu */
+	public static JMenu mkMenu(ResourceBundle b, String name) {
+		String menuLabel;
+		try { menuLabel = b.getString(name+".label"); }
+		catch (MissingResourceException e) { menuLabel=name; }
+		return new JMenu(menuLabel);
+	}
+
+	/** Convenience routine to make a JMenuItem */
+	public static JMenuItem mkMenuItem(ResourceBundle b,
+			String menu, String name) {
+
+		String miLabel;
+		try { miLabel = b.getString(menu + "." + name + ".label"); }
+		catch (MissingResourceException e) { miLabel=name; }
+		String key = null;
+		try { key = b.getString(menu + "." + name + ".key"); }
+		catch (MissingResourceException e) { key=null; }
+
+		// if (key == null)
+			return new JMenuItem(miLabel);
+		// else
+			// return new JMenuItem(miLabel, new MenuShortcut(key.charAt(0)));
+	}
+}
