ce23994bb4746184ae44b28c014bb12d209a7353
Ian Darwin <ian@darwinsys.com>
2000-10-30 17:56:42 +0000

    /tmp/to.commit

---
+++ src/com/darwinsys/lang/MutableInteger.java
@@ -0,0 +1,47 @@
+package com.darwinsys.util;
+
+/** A MutableInteger is like an Integer but mutable, to avoid the
+ * excess object creation involved in 
+ * c = new Integer(c.getInt()+1)
+ * which can get expensive if done a lot.
+ * Not subclassed from Integer, since Integer is final (for performance :-))
+ * @version $Id$
+ */
+public class MutableInteger {
+	private int value = 0;
+
+	public MutableInteger() {
+	}
+
+	public MutableInteger(int i) {
+		value = i;
+	}
+
+	public void incr() {
+		value++;
+	}
+
+	public void decr() {
+		value--;
+	}
+
+	public void setValue(int i) {
+		value = i;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public String toString() {
+		return Integer.toString(value);
+	}
+
+	public static String toString(int val) {
+		return Integer.toString(val);
+	}
+
+	public static int parseInt(String str) {
+		return Integer.parseInt(str);
+	}
+}
