327e109239ceecfa7e7aa3eb1046e1d84f3fe7b0
Ian Darwin <ian@darwinsys.com>
2000-10-31 14:08:14 +0000

    Move FileIO into utils.

---
+++ regress/com/darwinsys/io/FileIOTest.java
@@ -0,0 +1,15 @@
+import com.darwinsys.util.*;
+import java.io.*;
+
+public class FileIOTest {
+    public static void main(String av[]) {
+        FileIO f = new FileIO();
+		try {
+			f.copyFile("FileIO.java", "FileIO.bak");
+		} catch (FileNotFoundException e) {
+			System.err.println(e);
+		} catch (IOException e) {
+			System.err.println(e);
+		}
+    }
+}
--- src/com/darwinsys/io/FileIO.java
+++ src/com/darwinsys/io/FileIO.java
@@ -1,3 +1,5 @@
+package com.darwinsys.util;
+
 import java.io.*;
 
 /**
