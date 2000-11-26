b2ac5eb72c5b319eeac57aa7b38afd2d6df1aa08
Ian Darwin <ian@darwinsys.com>
2000-11-26 02:05:14 +0000

    Provide Reader/Writer versions of things. Add readLine(filename) which opens the file, reads the first line, and closes it.

--- src/com/darwinsys/io/FileIO.java
+++ src/com/darwinsys/io/FileIO.java
@@ -19,9 +19,8 @@ public class FileIO {
 	}
 
 	/** Copy a file from an opened InputStream to opened OutputStream */
-	public copyFile(InputStream is, OutputStream os) 
+	public void copyFile(InputStream is, OutputStream os) 
 	throws IOException {
-		long count = 0;		// the byte count
 		int b;				// the byte read from the file
 		while ((b = is.read()) != -1) {
 			os.write(b);
@@ -29,4 +28,32 @@ public class FileIO {
 		is.close();
 		os.close();
     }
+
+	/** Copy a file from an opened Reader to opened Writer */
+	public void copyFile(Reader is, Writer os) 
+	throws IOException {
+		int b;				// the byte read from the file
+		while ((b = is.read()) != -1) {
+			os.write(b);
+		}
+		is.close();
+		os.close();
+    }
+
+	/** Copy a file from a filename to a PrintWriter. */
+	public void copyFile(String inName, PrintWriter pw) 
+	throws FileNotFoundException, IOException {
+		BufferedReader is = new BufferedReader(new FileReader(inName));
+		copyFile(is, pw);
+	}
+
+	/** Open a file and read the first line from it. */
+	public String readLine(String inName)
+	throws FileNotFoundException, IOException {
+		BufferedReader is = new BufferedReader(new FileReader(inName));
+		String line = null;
+		line = is.readLine();
+		is.close();
+		return line;
+	}
 }
