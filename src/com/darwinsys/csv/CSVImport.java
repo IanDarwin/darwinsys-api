6b59688bb84346488b2e1edc56003479d81cd735
Ian Darwin <ian@darwinsys.com>
2001-02-26 19:00:49 +0000

    Formatting

--- src/com/darwinsys/csv/CSVImport.java
+++ src/com/darwinsys/csv/CSVImport.java
@@ -15,7 +15,7 @@ public class CSV {
 
 	public static final String SEP = ",";
 
-	/** Construct a CSV parser */
+	/** Construct a CSV parser, with the default separator. */
 	public CSV() {
 		this(SEP);
 	}
@@ -31,69 +31,69 @@ public class CSV {
 	protected Vector list = new Vector();		// field Strings
 	protected String fieldsep;		// separator characters
 
-/** split: split line into fields
- * @return java.util.Enumeration containing each field as a String, in order.
- */
-public Enumeration split(String line)
-{
-	StringBuffer fld = new StringBuffer();
-	list.removeAllElements();			// discard previous, if any
-	int i = 0, j;
+	/** split: split line into fields
+	 * @return java.util.Enumeration containing each field as a String, in order.
+	 */
+	public Enumeration split(String line)
+	{
+		StringBuffer fld = new StringBuffer();
+		list.removeAllElements();			// discard previous, if any
+		int i = 0, j;
 
-	if (line.length() == 0) {
-		list.addElement(line);
+		if (line.length() == 0) {
+			list.addElement(line);
+			return list.elements();
+		}
+
+		do {
+			fld.setLength(0);
+			if (i < line.length() && line.charAt(i) == '"')
+				j = advquoted(line, fld, ++i);	// skip quote
+			else
+				j = advplain(line, fld, i);
+			list.addElement(fld.toString());
+			i = j + 1;
+		} while (j < line.length());
+
 		return list.elements();
 	}
 
-	do {
-		fld.setLength(0);
-		if (i < line.length() && line.charAt(i) == '"')
-			j = advquoted(line, fld, ++i);	// skip quote
-		else
-			j = advplain(line, fld, i);
-		list.addElement(fld.toString());
-		i = j + 1;
-	} while (j < line.length());
+	/** advquoted: quoted field; return index of next separator */
+	protected int advquoted(String s, StringBuffer fld, int i)
+	{
+		int j;
 
-	return list.elements();
-}
-
-/** advquoted: quoted field; return index of next separator */
-protected int advquoted(String s, StringBuffer fld, int i)
-{
-	int j;
-
-	for (j = i; j < s.length(); j++) {
-		// found end of field if find unescaped quote.
-		if (s.charAt(j) == '"' && s.charAt(j-1) != '\\') {
-			int k = s.indexOf(fieldsep, j);
-			System.out.println("j="+j+", k="+k);
-			if (k == -1)	// no separator found after this field
-				k += s.length();
-			for (k -= j; k-- > 0; )
-				fld.append(s.charAt(j++));
-			break;
+		for (j = i; j < s.length(); j++) {
+			// found end of field if find unescaped quote.
+			if (s.charAt(j) == '"' && s.charAt(j-1) != '\\') {
+				int k = s.indexOf(fieldsep, j);
+				System.out.println("j="+j+", k="+k);
+				if (k == -1)	// no separator found after this field
+					k += s.length();
+				for (k -= j; k-- > 0; )
+					fld.append(s.charAt(j++));
+				break;
+			}
+			fld.append(s.charAt(j));
 		}
-		fld.append(s.charAt(j));
-	}
-	return j;
-}
-
-/** advplain: unquoted field; return index of next separator */
-protected int advplain(String s, StringBuffer fld, int i)
-{
-	int j;
-
-	j = s.indexOf(fieldsep, i); // look for separator
-	// System.out.println("i="+i+", j="+j);
-	if (j == -1) {               	// none found
-		fld.append(s.substring(i));
-		return s.length();
-	} else {
-		fld.append(s.substring(i, j));
 		return j;
 	}
-}
+
+	/** advplain: unquoted field; return index of next separator */
+	protected int advplain(String s, StringBuffer fld, int i)
+	{
+		int j;
+
+		j = s.indexOf(fieldsep, i); // look for separator
+		// System.out.println("i="+i+", j="+j);
+		if (j == -1) {               	// none found
+			fld.append(s.substring(i));
+			return s.length();
+		} else {
+			fld.append(s.substring(i, j));
+			return j;
+		}
+	}
 
 	/** Canonical main program - test lines from a file and print. */
 	public static void main(String[] argv) throws IOException
