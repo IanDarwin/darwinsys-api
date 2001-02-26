0fc0b920a667ab58e45598c0ad1fafefdf15094c
Ian Darwin <ian@darwinsys.com>
2001-02-26 22:46:39 +0000

    new

---
+++ regress/com/darwinsys/csv/CSVSimple.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+/* Simple demo of CSV parser class.
+ */
+public class CSVSimple {	
+	public static void main(String[] args) {
+		CSV parser = new CSV();
+		Iterator it = parser.parse(
+			"\"LU\",86.25,\"11/4/1998\",\"2:19PM\",+4.0625");
+		while (it.hasNext()) {
+			System.out.println(it.next());
+		}
+	}
+}
