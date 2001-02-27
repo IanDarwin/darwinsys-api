2d2ef681f5666a381b88360681c5bd2aefe8920c
Ian Darwin <ian@darwinsys.com>
2001-02-27 02:42:52 +0000

    Getting closer to CSVRE.

--- src/com/darwinsys/csv/CSVRE.java
+++ src/com/darwinsys/csv/CSVRE.java
@@ -1,26 +1,32 @@
 import java.io.*;
-import java.util.*;
+import org.apache.regexp.*;
 
 /* Simple demo of CSV using Regular Expressions.
  */
-public class CSVSimple {	
+public class CSVRE {	
+	/** The rather involved pattern used to match CSV's */
+	public static final String CSV_PATTERN = "([^\"\\\\]*(\\\\.[^\"\\\\])*)*\",?)|([^,]+),?|,";
+
 	public static void main(String[] argv) throws IOException
 	{
 		String line;
 	
 		// Construct a new Regular Expression parser.
-		RE csv = new RE("([^\"\\]*(?);
+		RE csv = new RE(CSV_PATTERN);
 
+		// Construct an input reader
 		BufferedReader is = new BufferedReader(
 			new InputStreamReader(System.in));
 
 		while ((line = is.readLine()) != null) {
 			System.out.println("line = `" + line + "'");
-			Iterator e = csv.match(line);
 			int i = 0;
-			while (e.hasNext()) 
+			while (csv.match(line)) {
+				int start = csv.getParenStart();
+				int end   = csv.getParenEnd();
+
 				System.out.println("fields[" + i++ + "] = `" +
-					e.next() + "'");
+					fields[i] + "'");
 		}
 	}
 }
