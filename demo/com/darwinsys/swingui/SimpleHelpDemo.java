8cfa841359ae726c83ab0f13347f35c48050b6f0
Ian Darwin <ian@darwinsys.com>
2001-03-12 00:39:34 +0000

    Oops, it has to go into the package!

--- demo/com/darwinsys/swingui/SimpleHelpDemo.java
+++ demo/com/darwinsys/swingui/SimpleHelpDemo.java
@@ -1,6 +1,6 @@
 public class SimpleHelpDemo {
 	/** Main */
 	public static void main(String a[]) {
-		new SimpleHelp("TESTING").setVisible(true);
+		new com.darwinsys.util.SimpleHelp("TESTING").setVisible(true);
 	}
 }
