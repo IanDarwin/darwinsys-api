5031dc351d88d4501664e9a68cfd9fb0d6de13f0
Ian Darwin <ian@darwinsys.com>
2001-04-11 21:45:06 +0000

    Mid-week paranoia/backup commit

---
+++ src/com/darwinsys/html/Tag.java
@@ -0,0 +1,37 @@
+package com.darwinsys.util;
+
+import java.io.*;
+
+/** A series of methods for writing HTML/XML tags.
+ * All methods are static for ease of use.
+ */
+public class Tag {
+	protected static final char LB = '<';
+	protected static final char RB = '>';
+	protected static final char END = '/';
+
+	/** Output an empty tag */
+	public static void dotag(PrintWriter out, String tag) {
+		startTag(out, tag);
+		endTag(out, tag);
+	}
+
+	/** Output an body-content tag */
+	public static void doTag(PrintWriter out, String tag, String content) {
+		startTag(out, tag);
+		out.println(content);
+		endTag(out, tag);
+	}
+
+	/** Output a start tag */
+	public static void startTag(PrintWriter out, String tag) {
+		out.print(
+			new StringBuffer(LB).append(tag).append(RB).toString());
+	}
+
+	/** Output an end tag */
+	public static void endTag(PrintWriter out, String tag) {
+		out.print(
+			new StringBuffer(LB).append(END).append(tag).append(RB).toString());
+	}
+}
