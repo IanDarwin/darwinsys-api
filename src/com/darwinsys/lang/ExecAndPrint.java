283b9315d4e2f1b0ace4658b6207e91208c2af86
Ian Darwin <ian@darwinsys.com>
2001-04-29 18:08:10 +0000

    ExecAndPrint: runtime.exec(cmd) and print the output.

---
+++ src/com/darwinsys/lang/ExecAndPrint.java
@@ -0,0 +1,56 @@
+package com.darwinsys.util;
+
+import java.io.*;
+
+/**
+ * ExecAndPrint runs a program using Runtime.exec(),
+ * read the program's output, and returns its exit status.
+ */
+public class ExecAndPrint {
+
+	/** Need a Runtime object for any of these methods */
+	protected static Runtime r = Runtime.getRuntime();
+
+	/** Run the command given as a String, printing its output to System.out */
+	protected static int run(String cmd) throws IOException { 
+		return run(cmd, new OutputStreamWriter(System.out));
+	}
+
+	/** Run the command given as a String, print its output to "out" */
+	protected static int run(String cmd, Writer out) throws IOException { 
+
+		String line;
+		
+		Process p = r.exec(cmd);
+
+		FileIO.copyFile(new InputStreamReader(p.getInputStream()), out, false);
+		try {
+			p.waitFor();	// wait for process to complete
+		} catch (InterruptedException e) {
+			return -1;
+		}
+		return p.exitValue();
+	}
+
+	/** Run the command given as a String[], print its output to System.out */
+	protected static int run(String[] cmd) throws IOException { 
+		return run(cmd, new OutputStreamWriter(System.out));
+	}
+
+	/** Run the command given as a String[], print its output to "out" */
+	protected static int run(String[] cmd, Writer out) throws IOException { 
+
+		String line;
+		
+		Process p = r.exec(cmd);
+
+		FileIO.copyFile(new InputStreamReader(p.getInputStream()), out, false);
+
+		try {
+			p.waitFor();	// wait for process to complete
+		} catch (InterruptedException e) {
+			return -1;
+		}
+		return p.exitValue();
+	}
+}
