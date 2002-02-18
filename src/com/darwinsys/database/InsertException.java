1e2b7d9fcc514533f0dc6bfb90efcb05ce553502
Ian Darwin <ian@darwinsys.com>
2002-02-18 15:18:57 +0000

    CatalogServlet: use US locale, use "application" instead of multiple calls to getServletContext(); add logic for the slightly redundant searchtype=search. Product: move all fields to the top; fill in most missing accessors. ProductAccessor: split constructor to allow calling with Connection, add selectBySubCategory logic (method searchProducts, for when we'll ever be that big), move COMMON_FIELDS to be a field; add addProduct() method, stealing code from DataEntryClient (trying to get all SQL out of client).

---
+++ src/com/darwinsys/database/InsertException.java
@@ -0,0 +1,9 @@
+/** Checked exception for failed insertions */
+public class InsertException extends java.lang.RuntimeException {
+	public InsertException() {
+		super();
+	}
+	public InsertException(String msg) {
+		super(msg);
+	}
+}
