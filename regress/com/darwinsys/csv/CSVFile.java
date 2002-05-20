ca1520a24da58005efacc69e04e5673d8e5b85c7
Ian Darwin <ian@darwinsys.com>
2002-05-20 18:26:25 +0000

    Make a filter, so can read named files. Change data to be less scary.

--- regress/com/darwinsys/csv/CSVFile.java
+++ regress/com/darwinsys/csv/CSVFile.java
@@ -4,15 +4,24 @@ import java.util.*;
 /** CSV in action: lines from a file and print. */
 public class CSVFile {
 
-	public static void main(String[] argv) throws IOException
-	{
-		String line;
+	public static void main(String[] args) throws IOException {
 	
 		// Construct a new CSV parser.
 		CSV csv = new CSV();
 
-		BufferedReader is = new BufferedReader(
-			new InputStreamReader(System.in));
+		if (args.length == 0) {	// read standard input
+			BufferedReader is = new BufferedReader(
+				new InputStreamReader(System.in));
+			process(csv, is);
+		} else {
+			for (int i=0; i<args.length; i++) {
+				process(csv, new BufferedReader(new FileReader(args[i])));
+			}
+		}
+	}
+
+	protected static void process(CSV csv, BufferedReader is) throws IOException {
+		String line;
 		while ((line = is.readLine()) != null) {
 			System.out.println("line = `" + line + "'");
 			Iterator e = csv.parse(line);
