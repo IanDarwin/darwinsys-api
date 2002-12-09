1d9c2aa9fcaeffeefeeedad8c8b4ea6c595fce5e
Ian Darwin <ian@darwinsys.com>
2002-12-09 04:39:30 +0000

    CalIcon and Clock are in package com.darwinsys.util. RE-1.4 upated. build.xml updated. Easter.java major facelift to provide a static method.

--- src/com/darwinsys/swingui/CalIcon.java
+++ src/com/darwinsys/swingui/CalIcon.java
@@ -1,16 +1,19 @@
-/** Display one of those standard Calendar Page icons with
- * Weekday, Day and Month.
- * <P>TODO:
- * <BR>1) Test with/without showTime.
- * <BR>2) MouseListener to call a callback on single click.
- * @author	Ian Darwin, ian@darwinsys.com
- * @version $Id$
- */
+package com.darwinsys.util;
 
 import java.awt.*;
 import java.util.*;
+import javax.swing.*;
 
-public class CalIcon extends javax.swing.JComponent {
+/** Display one of those standard Calendar Page icons with
+ * Weekday, Day and Month. Can be used as the Icon in a
+ * JButton. Can include or exclude an updating Clock at the top
+ * (invoke constructor with value of true to include).
+ * However, it should be excluded when using as an Icon, and 
+ * true when using as a Component.
+ * @author	Ian Darwin, http://www.darwinsys.com
+ * @version $Id$
+ */
+public class CalIcon extends JComponent implements Icon {
 	/** The size shalle be 64x64. */
 	protected final int SIZE = 64;
 	protected final Dimension d = new Dimension(SIZE, SIZE);
@@ -40,10 +43,17 @@ public class CalIcon extends javax.swing.JComponent {
 	protected FontMetrics monNameFM;
 
 	/** Construct the object with default arguments */
-	public CalIcon() {
+	public CalIcon(boolean showT) {
+		this(Calendar.getInstance(), showT);
+	}
+
+	/** Construct the object with a Calendar object */
+	public CalIcon(Calendar c, boolean showT) {
 		super();
+		showTime = showT;
+		myCal = c;
+
 		setLayout(null);			// we don't need another layout, ...
-		myCal = Calendar.getInstance();
 
 		if (showTime) {
 			// System.err.println("Constructing and adding Clock");
@@ -52,22 +62,19 @@ public class CalIcon extends javax.swing.JComponent {
 			clock.setBounds(0, 2, SIZE, 10);
 			// clock.setBackground(Color.black);
 			// clock.setForeground(Color.green);
+			RBY = d.height - (RBH+(showTime?12:0)/2);
+		} else {
+			RBY = 6;
 		}
+		RBX = 12;	// raised box x offset
+		// System.err.println("RBX, RBY = " + RBX + "," + RBY);
+
 		dayNumbFont = new Font("Serif",     Font.BOLD, 20);
 		dayNumbFM = getFontMetrics(dayNumbFont);
 		dayNameFont = new Font("SansSerif", Font.PLAIN, 10);
 		dayNameFM = getFontMetrics(dayNameFont);
 		monNameFont = new Font("SansSerif", Font.ITALIC, 10);
 		monNameFM = getFontMetrics(monNameFont);
-		RBX = 12;	// raised box x offset
-		RBY = d.height - (RBH+(showTime?12:0)/2);
-		// System.err.println("RBX, RBY = " + RBX + "," + RBY);
-	}
-
-	/** Construct the object with a Calendar object */
-	public CalIcon(Calendar c) {
-		this();
-		myCal = c;
 	}
 
 	/** Days of the week */
@@ -77,10 +84,16 @@ public class CalIcon extends javax.swing.JComponent {
 		"JUL", "AUG", "SEP", "OCT", "NOV", "DEC",
 	};
 
-	/** Paint: draw the calendar page. */
+	/** Paint: draw the calendar page in the JComponent.
+	 * Delegates most work to paintIcon().
+	 */
 	public void paint(Graphics g) {
-		String s;
-		int w;
+
+		paintIcon(this, g, 0, 0);
+	}
+
+	/** paintIcon: draw the calendar page. */
+	public void paintIcon(Component c, Graphics g, int x, int y) {
 
 		// Allow clock to get painted (voodoo magic)
 		if (showTime)
@@ -88,36 +101,52 @@ public class CalIcon extends javax.swing.JComponent {
 
 		// Outline it.
 		g.setColor(Color.black);
-		g.draw3DRect(0, 0, d.width-2, d.height-2, true);
+		g.draw3DRect(x, y, d.width-2, d.height-2, true);
 
 		// Show the date: First, a white page with a drop shadow.
 		g.setColor(Color.gray);
-		g.fillRect(RBX+3, RBY+3, RBW, RBH);
+		g.fillRect(x + RBX+3, y + RBY+3, RBW, RBH);
 		g.setColor(Color.white);
-		g.fillRect(RBX, RBY, RBW, RBH);
+		g.fillRect(x + RBX, y + RBY, RBW, RBH);
 
-		g.setColor(getForeground());
+		// g.setColor(getForeground());
+		g.setColor(Color.black);
 
-		s = days[myCal.get(Calendar.DAY_OF_WEEK)-1];
+		String s = days[myCal.get(Calendar.DAY_OF_WEEK)-1];
 		g.setFont(dayNameFont);
-		w = dayNameFM.stringWidth(s);
-		g.drawString(s, RBX+((RBW-w)/2), RBY+10);
+		int w = dayNameFM.stringWidth(s);
+		g.drawString(s, x + RBX+((RBW-w)/2), y + RBY+10);
 
 		s = Integer.toString(myCal.get(Calendar.DAY_OF_MONTH));
 		g.setFont(dayNumbFont);
 		w = dayNumbFM.stringWidth(s);
-		g.drawString(s, RBX+((RBW-w)/2), RBY+25);
+		g.drawString(s, x + RBX+((RBW-w)/2), y + RBY+25);
 
 		s = mons[myCal.get(Calendar.MONTH)];
 		g.setFont(monNameFont);
 		w = monNameFM.stringWidth(s);
-		g.drawString(s, RBX+((RBW-w)/2), RBY+35);
+		g.drawString(s, x + RBX+((RBW-w)/2), y + RBY+35);
 	}
+
+	public int getIconWidth() { return SIZE; }
+	public int getIconHeight() { return SIZE; }
 
 	public Dimension getPreferredSize() {
 		return d;
 	}
 	public Dimension getMinimumSize() {
 		return d;
+	}
+
+	public static void main(String[] args) {
+		JFrame jf = new JFrame("Calendar");
+		Container cp = jf.getContentPane();
+		cp.setLayout(new GridLayout(0,1,5,5));
+		CalIcon c = new CalIcon(true);
+		cp.add(c);
+		JButton j = new JButton("As Icon", new CalIcon(false));
+		cp.add(j);
+		jf.pack();
+		jf.setVisible(true);
 	}
 }
