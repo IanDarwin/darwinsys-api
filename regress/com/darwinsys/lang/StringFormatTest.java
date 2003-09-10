d946b9e4070d13df18ba845f644749ec49b7f181
Ian Darwin <ian@darwinsys.com>
2003-09-10 01:10:01 +0000

    Rewrite as junit TestCase

--- regress/com/darwinsys/lang/StringFormatTest.java
+++ regress/com/darwinsys/lang/StringFormatTest.java
@@ -1,32 +1,28 @@
 package regress;
 
+import junit.framework.*;
+
 import com.darwinsys.lang.StringFormat;
 
-public class StringFormatTest {
-	/** Simple test cases; try even and odd lengths */
-	public static void main(String[] argv) {
-		String[] mesg = {"JavaFun", "JavaFun!" };
-		for (int i=0; i<mesg.length; i++) {
-			System.out.println("Input String " + mesg[i]);
-			dump(StringFormat.JUST_LEFT,
-				new StringFormat(5, StringFormat.JUST_LEFT).format(mesg[i]));
-			dump(StringFormat.JUST_LEFT,
-				new StringFormat(10, StringFormat.JUST_LEFT).format(mesg[i]));
-			dump(StringFormat.JUST_CENTER,
-				new StringFormat(5, StringFormat.JUST_CENTER).format(mesg[i]));
-			dump(StringFormat.JUST_CENTER,
-				new StringFormat(10, StringFormat.JUST_CENTER).format(mesg[i]));
-			dump(StringFormat.JUST_RIGHT,
-				new StringFormat(5, StringFormat.JUST_RIGHT).format(mesg[i]));
-			dump(StringFormat.JUST_RIGHT,
-				new StringFormat(10, StringFormat.JUST_RIGHT).format(mesg[i]));
-		}
+public class StringFormatTest extends TestCase {
+	String mesg = "JavaFun";
+
+	public void testLeft() {
+		assertEquals("JavaF",
+			new StringFormat(5, StringFormat.JUST_LEFT).format(mesg));
+		assertEquals("JavaFun   ",
+			new StringFormat(10, StringFormat.JUST_LEFT).format(mesg));
 	}
-	private static void dump(int format, String s) {
-		System.out.print((char)format);
-		System.out.print(" ==> \"");
-		System.out.print(s);
-		System.out.print('"');
-		System.out.println();
+	public void testCentre() {
+		assertEquals("JavaF",
+			new StringFormat(5, StringFormat.JUST_CENTER).format(mesg));
+		assertEquals(" JavaFun  ",
+			new StringFormat(10, StringFormat.JUST_CENTER).format(mesg));
+	}
+	public void testRight() {
+		assertEquals("JavaF",
+			new StringFormat(5, StringFormat.JUST_RIGHT).format(mesg));
+		assertEquals("  JavaFun",
+			new StringFormat(10, StringFormat.JUST_RIGHT).format(mesg));
 	}
 }
