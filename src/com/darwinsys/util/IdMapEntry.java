7c64e8da00198b0fb4bed8ec578d473786209299
Ian Darwin <ian@darwinsys.com>
2003-10-22 12:14:44 +0000

    Final version

--- src/com/darwinsys/util/IdMapEntry.java
+++ src/com/darwinsys/util/IdMapEntry.java
@@ -1,25 +1,26 @@
-package jabacart;
+package com.darwinsys.util;
 
 /**
- * An IdMapping is one int-String pair, for example, a primary key in a
- * database and a name or description of the item in the named row.
- * IdMapping objects are immutable.
+ * An IdMapEntry is one int-String pair, for example, an "id" or primary key
+ * in a database and a name or description of the item in the named row.
+ * IdMapEntry objects are immutable.
  * <p>Rather like a java.util.Map.Entry but without needing to convert.
  * @version $Id$
  */
-public class IdMapping {
+public class IdMapEntry {
 	int id;
 	String name;
-	public IdMapping(int id, String name) {
+
+	public IdMapEntry(int id, String name) {
 		this.id = id;
 		this.name = name;
 	}
 
-	public int getId() {
+	public int getKey() {
 		return id;
 	}
 
-	public String getName() {
+	public String getValue() {
 		return name;
 	}
 }
