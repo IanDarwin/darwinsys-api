0f9ac51e33e25779f99033a99a1a8a4bd2412c64
Ian Darwin <ian@darwinsys.com>
2003-12-17 02:11:34 +0000

    New: EnumerationIterator. Like ArrayIterator, an adapter, this one from old Enumeration to newer Iterator.

---
+++ src/com/darwinsys/util/EnumerationIterator.java
@@ -0,0 +1,42 @@
+package com.darwinsys.util;
+
+import java.util.Iterator;
+import java.util.Enumeration;
+
+/** A GOF Adapter to make instances of old Enumeration interface
+ * behave like new Iterator interface, so we only have to deal
+ * with one well-defined implementation of the Iterator pattern.
+ */
+public class EnumerationIterator implements Iterator {
+
+	/** The Enumeration being delegated to */
+	private Enumeration old;
+
+	/** Construct an EnumerationIterator from an old-style Enumeration.
+	 * @param  old The Enumeration to be adapted.
+	 */
+	public EnumerationIterator(Enumeration old) {
+		this.old = old;
+	}
+
+	/** Fulfuls the general contract of Iterator.hasNext(), that is,
+	 * return true as long as there is at least one more item in
+	 * the Iterator.
+	 */
+	public boolean hasNext() {
+		return old.hasMoreElements();
+	}
+	/** Fulfuls the general contract of Iterator.next(), that is,
+	 * returns the next element in the Iterator.
+	 */
+	public Object next() {
+		return old.nextElement();
+	}
+
+	/** Remove is not implemented (optional method).
+	 * @throws java.lang.UnsupportedOperationException in all cases.
+	 */
+	public void remove() {
+		throw new UnsupportedOperationException("remove");
+	}
+}
