786711be459a7563aa13aebb462f8ccb3e66559a
Ian Darwin <ian@darwinsys.com>
2004-03-14 20:34:12 +0000

    Finish refactoring to be JFrame-based.

--- regress/com/darwinsys/swingui/layout/RelativeLayoutTest.java
+++ regress/com/darwinsys/swingui/layout/RelativeLayoutTest.java
@@ -3,12 +3,14 @@ package regress;
 import java.awt.*;
 import java.awt.event.*;
 
+import javax.swing.JFrame;
+
 import com.darwinsys.swingui.layout.RelativeLayout;
 
 /**
  * Simple class to non-exhaustively test out RelativeLayout layout manager.
  */
-public class RelativeLayoutTest extends Frame {
+public class RelativeLayoutTest extends JFrame {
 	Button adButton;	// adjust (dummy here)
 	Button qb;			// quit
 
@@ -25,9 +27,10 @@ public class RelativeLayoutTest extends Frame {
 	/** Construct a RelativeLayoutTest test program. */
 	public RelativeLayoutTest() {
 		super("RelativeLayout Tester");
-		setLayout(new RelativeLayout(300, 150));
-		add("80,20", adButton = new Button("MidWidth"));
-		add("150,75", qb = new Button("Quit"));
+		Container cp = getContentPane();
+		cp.setLayout(new RelativeLayout(300, 150));
+		cp.add("80,20", adButton = new Button("MidWidth"));
+		cp.add("150,75", qb = new Button("Quit"));
 		qb.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.exit(0);
