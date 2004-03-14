cfd5db2889467cf4c779db708e99ded2000cef30
Ian Darwin <ian@darwinsys.com>
2004-03-14 19:58:49 +0000

    Add 5-column test

--- regress/com/darwinsys/swingui/layout/EntryLayoutTest.java
+++ regress/com/darwinsys/swingui/layout/EntryLayoutTest.java
@@ -13,6 +13,11 @@ public class EntryLayoutTest {
 
 	/** "main program" method - construct and show */
 	public static void main(String[] av) {
+		testTwoCols();
+		testFiveCols();
+	}
+
+	static void testTwoCols() {
 		final JFrame f = new JFrame("EntryLayout Demonstration");
 		Container cp = f.getContentPane();
 		double widths[] = { .33, .66 };
@@ -24,6 +29,32 @@ public class EntryLayoutTest {
 		cp.add(new JLabel("Security Domain:", SwingConstants.RIGHT));
 		cp.add(new JTextField(20));
 		// cp.add(new JLabel("Monkey wrench in works"));
+		f.pack();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setLocation(200, 200);
+		f.setVisible(true);
+	}
+
+	static void testFiveCols() {
+		final JFrame f = new JFrame("EntryLayout Five Columns");
+		Container cp = f.getContentPane();
+		double widths[] = { .25, .33, .10, .10, .20 };
+		cp.setLayout(new EntryLayout(widths));
+		cp.add(new JLabel("Login:", SwingConstants.RIGHT));
+		cp.add(new JTextField(10));
+		cp.add(new JCheckBox());
+		cp.add(new JCheckBox());
+		cp.add(new JCheckBox());
+		cp.add(new JLabel("Password:", SwingConstants.RIGHT));
+		cp.add(new JPasswordField(20));
+		cp.add(new JCheckBox());
+		cp.add(new JCheckBox());
+		cp.add(new JCheckBox());
+		cp.add(new JLabel("Security Domain:", SwingConstants.RIGHT));
+		cp.add(new JTextField(20));
+		cp.add(new JCheckBox());
+		cp.add(new JCheckBox());
+		cp.add(new JCheckBox());
 		f.pack();
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setLocation(200, 200);
