996a48fa68ab097b2942997de7fb44d04fec20d8
Ian Darwin <ian@darwinsys.com>
2004-04-03 22:11:05 +0000

    New: encapsulation of lookup in db.properties file andcreation of Connection from the 4 parameters.

---
+++ src/com/darwinsys/sql/ConnectionUtil.java
@@ -0,0 +1,82 @@
+package com.darwinsys.sql;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import com.darwinsys.database.DataBaseException;
+
+/** Encapsulate the Connection-related operations that every 
+ * JDBC program seems to use.
+ */
+public class ConnectionUtil {
+	/** The default config filename, relative to ${user.home} */
+	public static final String DEFAULT_NAME = ".db.properties";
+	
+	/** The current config filename */
+	private static String configFileName =
+		System.getProperty("user.home") + File.separator + DEFAULT_NAME;
+
+	/** Get a Connection from a config. */
+	public static Connection getConnection(String config) throws DataBaseException {
+		try {
+			Properties p = new Properties();
+			p.load(new FileInputStream(configFileName));
+			String db_driver = p.getProperty(config  + "." + "DBDriver");
+			String db_url = p.getProperty(config  + "." + "DBURL");
+			String db_user = p.getProperty(config  + "." + "DBUser");
+			String db_password = p.getProperty(config  + "." + "DBPassword");
+			if (db_driver == null || db_url == null) {
+				throw new IllegalStateException("Driver or URL null: " + config);
+			}
+			return createConnection(db_driver, db_url, db_user, db_password);
+		} catch (ClassNotFoundException ex) {
+			throw new DataBaseException(ex.toString());
+		} catch (IOException ex) {
+			throw new DataBaseException(ex.toString());
+		} catch (SQLException ex) {
+			throw new DataBaseException(ex.toString());
+		}
+	}
+
+	public static Connection createConnection(String db_driver, String db_url, 
+					String db_user, String db_password)
+			throws ClassNotFoundException, SQLException {
+
+
+		// Load the database driver
+		System.out.println("Loading driver " + db_driver);
+		Class.forName(db_driver);
+
+		System.out.println("Connecting to DB " + db_url);
+		return DriverManager.getConnection(
+			db_url, db_user, db_password);
+	}
+	
+	/** Returns the full path of the configuration file being used.
+	 * @return Returns the configFileName.
+	 */
+	public static String getConfigFileName() {
+		return configFileName;
+	}
+	
+	/** Sets the full path of the config file to read.
+	 * @param configFileName The configFileName to set.
+	 */
+	public static void setConfigFileName(String configFileName) {
+		File file = new File(configFileName);
+		if (!file.canRead()) {
+			throw new IllegalArgumentException("Unreadable: " + configFileName);
+		}
+		try {
+			ConnectionUtil.configFileName = file.getCanonicalPath();
+		} catch (IOException ex) {
+			System.err.println("Warning: IO error checking path: " + configFileName);
+			ConnectionUtil.configFileName = configFileName;
+		}
+	}
+}
