0aa1b2cc01c7b1b41b4f2b93f19c8b74220528d9
Ian Darwin <ian@darwinsys.com>
2004-05-24 17:57:49 +0000

    Sync with my version in OpenBSD; move to com.darwinsys packages

--- src/com/darwinsys/util/ScaledNumberFormat.java
+++ src/com/darwinsys/util/ScaledNumberFormat.java
@@ -1,3 +1,5 @@
+package com.darwinsys.util;
+
 import java.text.DecimalFormat;
 import java.text.FieldPosition;
 import java.text.Format;
@@ -6,19 +8,19 @@ import java.text.ParsePosition;
 /**
  * Format numbers scaled for human comprehension.
  *
- * "Human-readable" output uses 3 digits max.--put unit suffixes at
+ * "Human-readable" output uses43 digits max, and puts unit suffixes at
  * the end.  Makes output compact and easy-to-read esp. on huge disks.
  * Formatting code was originally in OpenBSD "df", converted to library routine.
- * Scanning code written for OpenBSD libutil by Todd Miller.
+ * Scanning code written by Ian Darwin, originally for OpenBSD libutil.
  * 
- * Rewritten in Java in January, 2001.
+ * Rewritten in Java in January, 2001; re-synched with OpenBSD in 2004.
  *
  * @author Ian F. Darwin, http://www.darwinsys.com/
  * @version $Id$
  */
 public class ScaledNumberFormat extends Format {
 
-	final static int NONE = 0;
+	final static int NONE = 0;		// to become an enum for 1.5
 	final static int KILO = 1;
 	final static int MEGA = 2;
 	final static int GIGA = 3;
@@ -26,13 +28,10 @@ public class ScaledNumberFormat extends Format {
 	final static int PETA = 5;
 	final static int EXA  = 6;
 
-	DecimalFormat df5, df6;
+	DecimalFormat df;
 
 	public ScaledNumberFormat() {
-		df5 = new DecimalFormat("0");
-		df5.setMinimumIntegerDigits(5);
-		df6 = new DecimalFormat("0");
-		df6.setMinimumIntegerDigits(6);
+		df = new DecimalFormat("0");
 	}
 
 	/** The input scaling factors. All three arrays must be in same order. */
@@ -49,13 +48,17 @@ public class ScaledNumberFormat extends Format {
 		1125899906842624L,
 		1152921504606846976L
 	};
+	private final static long LLONG_MAX = scale_factors[6];
 
-	/* To prevent numeric overflow (Java doesn't yet have "long long") */
+	/* To prevent numeric overflow (Java doesn't need a "long long") */
 	static final int MAX_DIGITS = 10;
 
-	/** Parse a generic object, returning an Object */
+	/** Parse a generic object, returning an Object
+	 * @return a Long containing the value (value is always
+	 * integral, even though has a fractional part before scaling).
+	  */
 	public Object parseObject(String s, ParsePosition where) {
-		int i, neg = +1, fract_digits = 0;
+		int i, sign = 0, fract_digits = 0;
 		long scale_fact = 1, whole = 0;
 		long fpart = 0;
 
@@ -65,18 +68,30 @@ public class ScaledNumberFormat extends Format {
 		char[] b = s.trim().toCharArray();
 		int p = 0;	// the index into b; the # of chars we've scanned.
 		
-		/* Main loop: Scan digits, find sign & decimal point, if present.
-		 * We don't allow exponentials, so no e E f F g G etc.
-		 * Advance 'p' to end, to get scale factor. *
-		 */
-		if (b[p] == '-') {
-			neg = -1;
-			++p;
-		} else if (b[p] == '+') {
-			/* nothing much to do */
-			++p;
+		/* Then at most one leading + or - */
+		while (b[p] == '-' || b[p] == '+') {
+			if (b[p] == '-') {
+				if (sign != 0)
+					throw new NumberFormatException(
+							"Number " + s + " has more than one sign.");
+				sign = -1;
+				++p;
+			} else if (b[p] == '+') {
+				if (sign != 0)
+					throw new NumberFormatException(
+							"Number " + s + " has more than one sign.");
+				sign = +1;
+				++p;
+			}
 		}
+		
+		/* Main loop: Scan digits, find decimal point, if present.
+		 * We don't allow exponentials, so no scientific notation
+		 * (but note that E for Exa might look like e to some!).
+		 * Advance 'p' to end, to get scale factor.
+		 */
 		for (; p<b.length && (Character.isDigit(b[p]) || b[p]=='.'); ++p) {
+			int ndigits = 0;
 			if (b[p] == '.') {
 				if (fract_digits>0) {
 					throw new NumberFormatException(
@@ -85,15 +100,17 @@ public class ScaledNumberFormat extends Format {
 				fract_digits = 1;
 				continue;
 			}
-			if (fract_digits==0 && p > MAX_DIGITS) {
-				throw new IllegalArgumentException("Number too large");
-			}
+
 			i = (b[p]) - '0';			// whew! finally a digit we can use
-			if (fract_digits > 0) {
+			if (fract_digits > 0) {	// fractional digit
+				if (fract_digits >= MAX_DIGITS)
+					throw new NumberFormatException("Number too large");
 				fpart *= 10;
 				fpart += i;
 				++fract_digits;		// track for later scaling
-			} else {
+			} else {						// normal digit
+				if (++ndigits >= MAX_DIGITS)
+					throw new NumberFormatException("Number too large");
 				whole *= 10;
 				whole += i;
 			}
@@ -101,8 +118,10 @@ public class ScaledNumberFormat extends Format {
 		/* printf("whole=%qd, fpart %ld, fract_digits %ld\n",
 		 *	whole, fpart, fract_digits);
 		 */
-		whole *= neg;
-		fpart *= neg;
+		if (sign < 0) {
+			whole *= sign;
+			fpart *= sign;
+		}
 
 		/* If no scale factor given, we're done. fraction is discarded. */
 		if (p >= b.length) {
@@ -113,13 +132,22 @@ public class ScaledNumberFormat extends Format {
 		for (i = 0; i < scale_factors.length; i++) {
 			if (b[p] == scale_chars[i] ||
 				b[p] == Character.toLowerCase(scale_chars[i])) {
+				// XXX if digits after this, throw exception
 				scale_fact = scale_factors[i];
 				// scale whole part: easy
 				whole *= scale_fact;
-				// scale fractional part
-				fpart *= scale_fact;
-				for (i = 0; i < fract_digits - 1; i++)
+				/* truncate fpart so it does't overflow.
+				 * then scale fractional part.
+				 */
+				while (fpart >= LLONG_MAX / scale_fact) {
 					fpart /= 10;
+					fract_digits--;
+				}
+				fpart *= scale_fact;
+				if (fract_digits > 0) {
+					for (i = 0; i < fract_digits -1; i++)
+						fpart /= 10;
+				}
 				whole += fpart;
 				return new Long(whole);
 			}
@@ -163,7 +191,6 @@ public class ScaledNumberFormat extends Format {
 				break;
 			}
 		}
-		// printf("AFTER: unit %d, number %d, fract %d\n", unit, number, fract);
 
 		if (fract < 0)
 			fract = -fract;
@@ -173,20 +200,12 @@ public class ScaledNumberFormat extends Format {
 			fract /= 10;
 
 		if (number == 0)
-			return "     0B";
-		else if (number > 10) {
-			buf.append(df6.format(number)).append(scale_chars[unit]);
+			return "0B";
+		else if (unit == NONE || number >= 100 || number <= -100) {
+			buf.append(df.format(number)).append(scale_chars[unit]);
 		} else {
-			buf.append(df5.format(number)).append('.').
-				append(number>10?0:fract).append(scale_chars[unit]);
-		}
-
-		// replace leading zeros with spaces
-		for (int i = 0; i < buf.length(); i++) {
-			if (buf.charAt(i) == '0')
-				buf.setCharAt(i, ' ');
-			else
-				break;
+			buf.append(df.format(number)).append('.').
+				append(fract).append(scale_chars[unit]);
 		}
 
 		return buf.toString();
