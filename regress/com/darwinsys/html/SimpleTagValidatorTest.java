20fa2c9bd19a272ac3217c5bea7ac81a7086d282
Ian Darwin <ian@darwinsys.com>
2004-08-05 12:03:29 +0000

    New: simple HTML tester to ensure that only certain tags are present.

---
+++ regress/com/darwinsys/html/SimpleTagValidatorTest.java
@@ -0,0 +1,39 @@
+package regress;
+
+import com.darwinsys.html.SimpleTagValidator;
+
+import junit.framework.TestCase;
+
+/**
+ * Test SimpleTagValidator
+ * @version $Id$
+ */
+public class SimpleTagValidatorTest extends TestCase {
+	SimpleTagValidator val = new SimpleTagValidator();
+	
+	public void testValidate() {
+		System.out.println("Valid tags: '" + val.getTagsAsString(false) + "'");
+		try {
+			val.validate(null);
+		} catch (NullPointerException ex) {
+			System.out.println(" caught expected NPE");
+		}
+		assertTrue("empty string", val.validate(""));
+
+		assertTrue("variety of tags",
+			val.validate("<p><a href='foo'>Link</a><i></p>?"));
+	}
+	
+	public void testTagsAsStrings() {
+		String[] myTags = {"br", "i", "img" };
+		assertEquals("tagsAsStrings", "br, i, img",
+			new SimpleTagValidator(myTags).getTagsAsString(true));
+		assertEquals("tagsAsStrings", "br i img",
+				new SimpleTagValidator(myTags).getTagsAsString(false));
+	}
+	public void testFailures() {
+		assertFalse("imgtag", val.validate("<img size=0>"));
+		assertEquals("get bad tag info", "img", val.getFailedTag());
+	}
+
+}
---
+++ src/com/darwinsys/html/SimpleTagValidator.java
@@ -0,0 +1,95 @@
+package com.darwinsys.html;
+
+import java.util.regex.*;
+
+/**
+ * Check a String to ensure that it contains either no HTML tags, or only
+ * the HTML tags listed in OKTAGS (or the corresponding end-tags).
+ * @version $Id$
+ */
+public class SimpleTagValidator {
+	/** The default list of OK tags.
+	 * As of this writing it allows only a, b, br, em, i and p tags.
+	 */
+	public static final String[] OKTAGS = {
+			"a", "b", "br", "em", "i", "p"
+	};
+	private final String[] okTags;
+	
+	Pattern patt = Pattern.compile("</?(\\w+)");
+	private String failedTag = null;
+	
+	/** Construct a Validator using the default OK list */
+	public SimpleTagValidator() {
+		okTags = OKTAGS;
+	}
+	
+	/** Construct a Validator using a non-default OK list */
+	public SimpleTagValidator(String[] okTagList) {
+		okTags = okTagList;
+	}
+	
+	/** Validate a String that may contain HTML to ensure it contains
+	 * only the tags listed in the OK list.
+	 * @param s The HTML String to validate.
+	 * @return true iff s does not contain any non-OK strings.
+	 */
+	public boolean validate(String s) {
+		if (s == null)
+			throw new NullPointerException("validate: null input");
+		failedTag = null;
+		Matcher m = patt.matcher(s);
+		while (m.find()) {
+				String tagName = m.group(1);
+				System.out.println(tagName);
+				// If even one tag bad, we return false.
+				if (!okTag(tagName)) {
+					failedTag = tagName;
+					return false;
+				}
+		}
+		// No bad tags found...
+		return true;
+	}
+
+	/**
+	 * Check that the tag extracted is valid.
+	 * @param tagName The tag name, e.g., "p" from "<p align='centre'>" or "/p"
+	 * @return True  if the tagname is in the OK list.
+	 */
+	private boolean okTag(String tagName) {
+		for (int i = 0; i < okTags.length; i++) {
+			if (okTags[i].equalsIgnoreCase(tagName))
+				return true;
+		}
+		return false;
+	}
+	
+	/** Return the last tag that failed.
+	 * Usage example:
+	 * <code>System.out.printf("Invalid tag %s\n", val.getFailedTag());</code>
+	 * @return
+	 */
+	public String getFailedTag() {
+		return failedTag;
+	}
+	
+	/**
+	 * Return the list of valid tags as a single string.
+	 * Usage example:
+	 * <code>System.out.printf("Valid tags are %01s\n", val.tagsAsString());</code> 
+	 * @param useCommas True to include commas ("a, b, c"); false just space ("a b c").
+	 */
+	public String getTagsAsString(boolean useCommas) {
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < okTags.length; i++) {
+			sb.append(okTags[i]);
+			if (i < okTags.length-1) {
+				if (useCommas)
+					sb.append(',');
+				sb.append(' ');
+			}
+		}
+		return sb.toString();
+	}
+}
