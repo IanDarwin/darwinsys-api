e164aa9579b016adbe3cb18330987bd6cd6c89f3
Ian Darwin <ian@darwinsys.com>
2004-12-04 19:20:52 +0000

    Remove +, - and @ from default char set; makes better passwords.

--- src/com/darwinsys/security/PassPhrase.java
+++ src/com/darwinsys/security/PassPhrase.java
@@ -22,8 +22,7 @@ public class PassPhrase {
 		'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'M', 'N',
 		'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-		'2', '3', '4', '5', '6', '7', '8', '9',
-		'+', '-', '@', 
+		'2', '3', '4', '5', '6', '7', '8', '9', 
 	};
 
 	/* Generate a Password object with a random password. */
