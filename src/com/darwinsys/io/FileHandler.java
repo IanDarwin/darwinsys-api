1eccfb29d66dc5564e328c7f68632349d2a9dc26
Ian Darwin <ian@darwinsys.com>
2005-03-31 21:50:51 +0000

    visit(File f) now throws IOException

--- src/com/darwinsys/io/FileHandler.java
+++ src/com/darwinsys/io/FileHandler.java
@@ -1,10 +1,11 @@
 package com.darwinsys.io;
 
 import java.io.File;
+import java.io.IOException;
 
 /** Visitor interface for Crawler.
  * @version $Id$
  */
 public interface FileHandler {
-	public void visit(File f);
+	public void visit(File f) throws IOException;
 }
