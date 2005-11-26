6f080df6034247cc5d09ce9e5361a8494894399e
Ian Darwin <ian@darwinsys.com>
2005-11-26 01:55:25 +0000

    New: Manual test for PrefsPanel.

---
+++ regress/com/darwinsys/swingui/PrefsPanelTest.java
@@ -0,0 +1,21 @@
+package swingui;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import com.darwinsys.swingui.PrefsPanel;
+
+public class PrefsPanelTest  {
+	public static void main(String[] args) {
+		JFrame jf = new JFrame("PrefsPanelTest");
+		PrefsPanel p = new PrefsPanel();
+		jf.add(p);
+		p.add(PrefsPanel.PredefinedPanel.ONE_COLOR);
+		p.add(PrefsPanel.PredefinedPanel.ONE_FONT);
+		p.add(PrefsPanel.PredefinedPanel.FGBG_COLOR);
+		JPanel test = new JPanel();
+		p.add(test, "Test", null);
+		jf.pack();
+		jf.setVisible(true);
+	}
+}
