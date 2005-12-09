f0cac59a5358685974b38fd55a95999451ecbf14
Ian Darwin <ian@darwinsys.com>
2005-12-09 03:20:19 +0000

    Remove incorrect comment.

--- src/com/darwinsys/genericui/GenericUI.java
+++ src/com/darwinsys/genericui/GenericUI.java
@@ -4,8 +4,6 @@ public interface GenericUI {
 	
 	/** Display a popup to the user, given a Severity,
 	 * a titlebar title, and a message.
-	 * Implementations are encouraged to make this method static
-	 * for the user's convenience.
 	 * @param sev
 	 * @param title
 	 * @param message
