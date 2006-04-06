16b582606e7365b805fe05b96188a494dee84784
Ian Darwin <ian@darwinsys.com>
2006-04-06 22:34:01 +0000

    Provide accessors, to suppress complaints.

--- src/com/darwinsys/diff/DiffChunk.java
+++ src/com/darwinsys/diff/DiffChunk.java
@@ -1,8 +1,8 @@
 package com.darwinsys.diff;
 
 public class DiffChunk<T> {
-	DiffType type;
-	T data;
+	private DiffType type;
+	private T data;
 	
 	/**
 	 * @param data
@@ -12,5 +12,13 @@ public class DiffChunk<T> {
 		super();
 		this.data = data;
 		this.type = type;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public DiffType getType() {
+		return type;
 	}
 } 
