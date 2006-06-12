7fc0aec53a636a38f4880ccebbe461b625574469
Ian Darwin <ian@darwinsys.com>
2006-06-12 16:02:53 +0000

    Allow multiple columns, so change argument list to just table and "select command..."

--- src/com/darwinsys/sql/ValuePrompt.java
+++ src/com/darwinsys/sql/ValuePrompt.java
@@ -4,35 +4,34 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.Statement;
 
 /** Prompt for a value in a database.
- * Usage: ValuePrompt connection table column "expression"
+ * Usage: ValuePrompt connection "select expression"
  * e.g.
  * <pre>
- * java ValuePrompt mydb products location "location is null or location = 0"
+ * java ValuePrompt mydb "select title, product, location from products  where location is null or location = 0"
  * </pre>
  * @author ian
  */
 public class ValuePrompt {
 
 	public static final String USAGE =
-		"ValuePrompt connection-name table-name column-name \"expression\"";
+		"ValuePrompt connection-name \"select query...\"";
 
 	public static void main(String args[]) {
     	if (args.length == 1 && args[0].startsWith("-h")) {
     		System.err.println(USAGE);
     		System.exit(0);
     	}
-    	if (args.length != 4) {
+    	if (args.length != 2) {
     		System.err.println(USAGE);
     		System.exit(1);
     	}
     	
     	String connection = args[0];
-    	String table = args[1];
-    	String column = args[2];
-    	String expression = args[3];
+    	String query = args[1];
         
         try {
         	Connection con = ConnectionUtil.getConnection(connection);         
@@ -41,17 +40,23 @@ public class ValuePrompt {
         		new BufferedReader(new InputStreamReader(System.in));
 			Statement stmt = con.createStatement(
 			ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
-			String query = "SELECT * FROM " + table +
-					" where " + expression;
 			ResultSet rs = stmt.executeQuery(query);
 			System.out.println("Starting processing: query = " + query);
+			ResultSetMetaData rsMetaData = rs.getMetaData();
+			int colCount = rsMetaData.getColumnCount();
 			while (rs.next()) {
-				System.out.println(rs.getObject(1));
-				String resp = is.readLine();
-				if (resp == null || "".equals(resp))
-					continue;
-				rs.updateString(column, resp);
-				rs.updateRow();
+				boolean changed = false;
+				for (int i = 1; i <= colCount; i++) {
+					System.out.printf("%s=%s: ", rsMetaData.getColumnName(i), rs.getObject(i));
+					String resp = is.readLine();
+					if (resp == null || "".equals(resp))
+						continue;
+					rs.updateString(i, resp);
+					changed = true;
+				}
+				if (changed) {
+					rs.updateRow();
+				}
 			}
 			System.out.println("All done.");
 			rs.close();
