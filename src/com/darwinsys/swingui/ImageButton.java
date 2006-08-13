f8e77965ea9300484f8b211a27ea8978acb8362e
Ian Darwin <ian@darwinsys.com>
2006-08-13 15:56:07 +0000

    Deprecation, and just call JButton constructors directly.

--- src/com/darwinsys/swingui/ImageButton.java
+++ src/com/darwinsys/swingui/ImageButton.java
@@ -1,31 +1,23 @@
 package com.darwinsys.swingui;
 
-import java.awt.Insets;
-
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 /** A JButton with just a graphic display.
- * Adapted from the highly-recommended O'Reilly book Swing Hacks (ISBN 0-596-00907-0).
+ * Just a thin wrapper for JButton.
  * @author ian
- * @version $Id$
+ * @deprecated Use the appropriate JButton constructors directly.
  */
 public class ImageButton extends JButton {
 	private static final long serialVersionUID = 3904965248233714485L;
 
+	@Deprecated
     public ImageButton(ImageIcon icon) {
-        this(icon, null);
+        super(icon);
     }
-    
+
+	@Deprecated
 	public ImageButton(ImageIcon icon, String text) {
-		setIcon(icon);
-		setIconTextGap(0);
-		int width = icon.getImage().getWidth(null);
-		int height = icon.getImage().getHeight(null);
-		setSize(width, height);
-		setMargin(new Insets(0,0,0,0));
-		setBorderPainted(false);
-		setBorder(null);
-		setText(text);
+		super(text, icon);
 	}
 }
