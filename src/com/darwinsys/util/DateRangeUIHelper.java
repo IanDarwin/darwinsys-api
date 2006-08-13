8a2f818ea5048e7fad0ef57bb7f13aa8e7825157
Ian Darwin <ian@darwinsys.com>
2006-08-13 18:17:48 +0000

    Hide array behind public accessor.

--- src/com/darwinsys/util/DateRangeUIHelper.java
+++ src/com/darwinsys/util/DateRangeUIHelper.java
@@ -6,7 +6,7 @@ import java.util.Date;
 /**
  * Utilities for using choice items like "Modified in last..." and a list of { day, week, month...}.
  * Typical use is getDateRangeValues() to get the values, and getDateRangeLabels for the labels, when
- * displaying the choice item to the user, and pass the chosen value to getDateFromRange(). 
+ * displaying the choice item to the user, and pass the chosen value to getDateFromRange().
  * Alternately, if your UI allows components to be displayed directly (e.g., Swing), use the
  * public field dateRanges. For example, in Struts you might use this in a JSP:
  * <pre>
@@ -16,13 +16,13 @@ import java.util.Date;
  *       labelName="my.date" labelProperty="dateRangeLabels"
  *       />
  *
- *   
+ *
  *   // Swing Example:
  *   // Constructor:
  *   jComboBox.setListData(DateUtils.dateRanges);
  *   // Action Handler
  *   DateRangeUIHelper.Range r = (DateRangeUIHelpers.Range)jComboBox.getSelectedItem();
- *   Date startDate = DateRangeUIHelper.getDateFromRange(r.getChoiceValue()); 
+ *   Date startDate = DateRangeUIHelper.getDateFromRange(r.getChoiceValue());
  * </pre>
  * <p>For a runnable example, see regress.util.DateRangeUIHelper in the the darwinsys-api project.
  */
@@ -32,9 +32,9 @@ public class DateRangeUIHelper {
 	 * Tiny data holder for a range like "7d" and its numeric equivalent.
 	 */
 	public static class Range {
-		String choiceValue;
-		String choiceLabel;
-		int days;
+		final String choiceValue;
+		final String choiceLabel;
+		final int days;
 		public Range(String val, int days, String lab) {
 			super();
 			this.choiceValue = val;
@@ -58,7 +58,7 @@ public class DateRangeUIHelper {
 	/**
 	 * The list of Range values that this class uses.
 	 */
-	public static final Range[] dateRanges = {
+	static final Range[] dateRanges = {
 		new Range("1d",   1, "day"),
 		new Range("7d",   7, "week"),
 		new Range("1m",  31, "month"),
@@ -66,7 +66,11 @@ public class DateRangeUIHelper {
 		new Range("6m", 183, "six months"),
 		new Range("1y", 365, "year"),
 	};
-	
+
+	public static Range[] getDateRanges() {
+		return dateRanges.clone();
+	}
+
 	/** Return the &lt;choice&gt; values for use in a DropDown and in getDateFromRange(). */
 	public static String[] getDateRangeValues() {
 		String[] choices = new String[dateRanges.length];
@@ -75,7 +79,7 @@ public class DateRangeUIHelper {
 		}
 		return choices;
 	}
-	
+
 	/** Return the &lt;choice&gt; labels for use in a DropDown and in getDateFromRange(). */
 	public static String[] getDateRangeLabels() {
 		String[] choices = new String[dateRanges.length];
@@ -84,7 +88,7 @@ public class DateRangeUIHelper {
 		}
 		return choices;
 	}
-	
+
 	public static Date getDateFromRange(String dropdownValue) {
 		if (dropdownValue == null) {
 			throw new IllegalArgumentException("Logic Error: input argument may not be null");
@@ -100,13 +104,13 @@ public class DateRangeUIHelper {
 			throw new IllegalArgumentException(
 				String.format("Logic Error: Days dropdown value %s invalid (not one we provided)", dropdownValue));
 		}
-		
+
 		/** Today's date */
 		Calendar now = Calendar.getInstance();
-		
+
 		// Add a "# of days" increment (really a decrement) to the existing Calendar object
 		now.add(Calendar.DATE, -nDays);
-		
+
 		Date dateStart = now.getTime();
 		// System.out.println("New date is " + dateStart);
 		return dateStart;
