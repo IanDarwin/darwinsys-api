10275cbb5b546cee1ec243cf0878ef1d5616e580
Ian Darwin <ian@darwinsys.com>
2006-10-14 18:49:21 +0000

    A simple Date object (with no time, no deprecated baggage, etc.).

---
+++ src/com/darwinsys/util/DateSimple.java
@@ -0,0 +1,51 @@
+package com.darwinsys.util;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/**
+ * DateSimple represents YYYY-MM-DD without all the overhead and
+ * deprecated baggage of java.util.Date.
+ * Objects of this type are immutable.
+ */
+
+public class DateSimple {
+	int year;
+	int month;
+	int day;
+
+	/**
+	 * @param year
+	 * @param month
+	 * @param day
+	 */
+	public DateSimple(int year, int month, int day) {
+		super();
+		this.year = year;
+		this.month = month;
+		this.day = day;
+	}
+
+	public DateSimple() {
+		Calendar c = Calendar.getInstance();
+		year = c.get(Calendar.YEAR);
+		month  = c.get(Calendar.MONTH);
+		day = c.get(Calendar.DAY_OF_MONTH);
+	}
+
+	public int getDay() {
+		return day;
+	}
+
+	public int getMonth() {
+		return month;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public Calendar getCalendar() {
+		return new GregorianCalendar(year, month, day);
+	}
+}
