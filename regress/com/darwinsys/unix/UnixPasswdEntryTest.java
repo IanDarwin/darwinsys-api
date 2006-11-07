dd5aa6ecec1dc527bd15925a879d0bed4a4889d4
Ian Darwin <ian@darwinsys.com>
2006-11-07 19:20:35 +0000

    UNIX password entry (will not work on MS-Windows, sorry).

---
+++ regress/com/darwinsys/unix/UnixPasswdEntryTest.java
@@ -0,0 +1,42 @@
+package com.darwinsys.unix;
+
+import java.util.List;
+import java.util.regex.Matcher;
+
+import junit.framework.TestCase;
+
+public class UnixPasswdEntryTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	public void testRegEx() {
+		String sample = "foo:*:101:102:Foo Bar:/home/foo:/bin/ksh";
+		Matcher m = UnixPasswdEntry.patt.matcher(sample);
+		assertTrue("line matches", m.matches());
+		assertEquals("match name", "foo", m.group(1));
+		assertEquals("match dummy pw", "*", m.group(2));
+		assertEquals("match uid", "101", m.group(3));
+		assertEquals("match gid", "102", m.group(4));
+		assertEquals("match gecos", "Foo Bar", m.group(5));
+		assertEquals("match homedir", "/home/foo", m.group(6));
+		assertEquals("match shell", "/bin/ksh", m.group(7));
+
+	}
+
+	public void testGetPwEntriesString() throws Exception {
+		List<UnixPasswdEntry> pwEntries = UnixPasswdEntry.getPwEntries();
+		assertNotNull(pwEntries);
+		assertTrue("at least root & current user", pwEntries.size() > 2);
+		String loggedInAs = System.getProperty("user.name");
+		for (UnixPasswdEntry ent : pwEntries) {
+			if (loggedInAs.equals(ent.getName())) {
+				System.out.println("Current user = " + ent);
+				return;
+			}
+		}
+		fail("Did not find entry for current user name");
+	}
+
+}
---
+++ src/com/darwinsys/unix/UnixPasswdEntry.java
@@ -0,0 +1,152 @@
+package com.darwinsys.unix;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Structure of the readable password on a BSD system;
+ * on Linux your "mailage" may vary.
+ */
+public class UnixPasswdEntry {
+
+	public static final String DEFAULT_FILE_NAME = "/etc/passwd";
+
+	private String name; 		// User's login name.
+	private String dummyPassword; // User's encrypted password - only in shadow
+	private int uid; 			// User's login user ID.
+	private int	gid; 			// User's login group ID.
+	private String loginClass; 	// NOT PUBLIC User's general classification (see login.conf(5)).
+	private String change; 		// NOT PUBLIC Password change time.
+	private String expire; 		// NOT PUBLIC Account expiration time.
+	private String gecos; 		// General information about the user.
+	private String homeDir; 	// User's home directory.
+	private String shell; 		// User's login shell.
+
+	static final Pattern patt = Pattern.compile(
+		"(\\w+):(.*?):(\\d+):(\\d+):(.*?):([/\\w]+):([/\\w]*)");
+
+	public static List<UnixPasswdEntry> getPwEntries() throws IOException {
+		return getPwEntries(DEFAULT_FILE_NAME);
+	}
+
+    public static List<UnixPasswdEntry> getPwEntries(String fileName) throws IOException {
+    	List<UnixPasswdEntry> all = new ArrayList<UnixPasswdEntry>();
+    	BufferedReader rdr = new BufferedReader(new FileReader(fileName));
+    	String line;
+    	try {
+    		while ((line = rdr.readLine()) != null) {
+    			Matcher m = patt.matcher(line);
+    			if (!m.matches()) {
+    				throw new IOException("Line doesn't match RE " + line);
+    			}
+    			UnixPasswdEntry ent = new UnixPasswdEntry();
+    			ent.name = m.group(1);
+    			ent.dummyPassword = m.group(2);
+    			ent.uid = Integer.parseInt(m.group(3));
+    			ent.gid = Integer.parseInt( m.group(4));
+    			ent.gecos = m.group(5);
+    			ent.homeDir = m.group(6);
+    			ent.shell = m.group(7);
+    			all.add(ent);
+    		}
+    	} finally {
+    		if (rdr != null)
+    			rdr.close();
+    	}
+    	return all;
+    }
+
+    @Override
+    public String toString() {
+    	return String.format("pw_ent(%s, %d, %s, %s)", name, uid, gecos, homeDir);
+    }
+
+	public String getChange() {
+		return change;
+	}
+
+	public void setChange(String change) {
+		this.change = change;
+	}
+
+	public String getDummyPassword() {
+		return dummyPassword;
+	}
+
+	public void setDummyPassword(String dummyPassword) {
+		this.dummyPassword = dummyPassword;
+	}
+
+	public String getExpire() {
+		return expire;
+	}
+
+	public void setExpire(String expire) {
+		this.expire = expire;
+	}
+
+	public String getGecos() {
+		return gecos;
+	}
+
+	public void setGecos(String gecos) {
+		this.gecos = gecos;
+	}
+
+	public int getGid() {
+		return gid;
+	}
+
+	public void setGid(int gid) {
+		this.gid = gid;
+	}
+
+	public String getHomeDir() {
+		return homeDir;
+	}
+
+	public void setHomeDir(String homeDir) {
+		this.homeDir = homeDir;
+	}
+
+	public String getLoginClass() {
+		return loginClass;
+	}
+
+	public void setLoginClass(String loginClass) {
+		this.loginClass = loginClass;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getShell() {
+		return shell;
+	}
+
+	public void setShell(String shell) {
+		this.shell = shell;
+	}
+
+	public int getUid() {
+		return uid;
+	}
+
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+
+	public static Pattern getPattern() {
+		return patt;
+	}
+}
