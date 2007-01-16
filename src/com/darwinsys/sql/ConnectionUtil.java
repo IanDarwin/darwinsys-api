bb7737e9cd3140fdab923609d37ef9e404d41077
Ian Darwin <ian@darwinsys.com>
2007-01-16 18:49:36 +0000

    Minor cleanups. Corrected order of parameters in one getConnection()'s call to another.

--- src/com/darwinsys/sql/ConnectionUtil.java
+++ src/com/darwinsys/sql/ConnectionUtil.java
@@ -26,6 +26,30 @@ public class ConnectionUtil {
 	private static String configFileName =
 		System.getProperty("user.home") + File.separator + DEFAULT_NAME;
 
+	/** Sets the full path of the config file to read.
+	 * @param configFileNam The FileName of the configuration file to use.
+	 */
+	public static void setConfigFileName(String configFileNam) {
+		configFileName = configFileNam;
+		File file = new File(configFileName);
+		if (!file.canRead()) {
+			throw new IllegalArgumentException("Unreadable: " + configFileName);
+		}
+		try {
+			ConnectionUtil.configFileName = file.getCanonicalPath();
+		} catch (IOException ex) {
+			System.err.println("Warning: IO error checking path: " + configFileName);
+			ConnectionUtil.configFileName = configFileName;
+		}
+	}
+
+	/** Returns the full path of the configuration file being used.
+	 * @return Returns the configFileName.
+	 */
+	public static String getConfigFileName() {
+		return configFileName;
+	}
+
 	/** Get a Configuration for the given config using the default or set property file name */
 	public static  Configuration getConfiguration(String config) throws DataBaseException {
 		try {
@@ -42,11 +66,11 @@ public class ConnectionUtil {
 	 * @param config The name of the wanted configuration
 	 * @return The matching configuration
 	 */
-	private static Configuration getConfiguration(Properties p, String config) {
-		String db_driver = p.getProperty(config  + "." + "DBDriver");
-		String db_url = p.getProperty(config  + "." + "DBURL");
-		String db_user = p.getProperty(config  + "." + "DBUser");
-		String db_password = p.getProperty(config  + "." + "DBPassword");
+	static Configuration getConfiguration(final Properties p, final String config) {
+		final String db_url = p.getProperty(config  + "." + "DBURL");
+		final String db_driver = p.getProperty(config  + "." + "DBDriver");
+		final String db_user = p.getProperty(config  + "." + "DBUser");
+		final String db_password = p.getProperty(config  + "." + "DBPassword");
 		if (db_driver == null || db_url == null) {
 			throw new DataBaseException("Driver or URL null: " + config);
 		}
@@ -54,9 +78,9 @@ public class ConnectionUtil {
 	}
 
 	/** Get a Connection for the given config using the default or set property file name */
-	public static Connection getConnection(String configName) throws DataBaseException {
+	public static Connection getConnection(final String configName) throws DataBaseException {
 		try {
-			Properties p = new Properties();
+			final Properties p = new Properties();
 			p.load(new FileInputStream(configFileName));
 			return getConnection(p, configName);
 		} catch (IOException ex) {
@@ -67,8 +91,8 @@ public class ConnectionUtil {
 	/** Get a Connection for the given config name from a provided Properties */
 	public static Connection getConnection(Properties p,  String configName) throws DataBaseException {
 		try {
-			String db_driver = p.getProperty(configName  + "." + "DBDriver");
 			String db_url = p.getProperty(configName  + "." + "DBURL");
+			String db_driver = p.getProperty(configName  + "." + "DBDriver");
 			String db_user = p.getProperty(configName  + "." + "DBUser");
 			String db_password = p.getProperty(configName  + "." + "DBPassword");
 			if (db_driver == null || db_url == null) {
@@ -97,31 +121,7 @@ public class ConnectionUtil {
 	}
 
 	public static Connection getConnection(Configuration c) throws ClassNotFoundException, SQLException {
-		return getConnection(c.dbDriverName, c.dbURL, c.dbUserName, c.dbPassword);
-	}
-
-	/** Returns the full path of the configuration file being used.
-	 * @return Returns the configFileName.
-	 */
-	public static String getConfigFileName() {
-		return configFileName;
-	}
-
-	/** Sets the full path of the config file to read.
-	 * @param configFileNam The FileName of the configuration file to use.
-	 */
-	public static void setConfigFileName(String configFileNam) {
-		configFileName = configFileNam;
-		File file = new File(configFileName);
-		if (!file.canRead()) {
-			throw new IllegalArgumentException("Unreadable: " + configFileName);
-		}
-		try {
-			ConnectionUtil.configFileName = file.getCanonicalPath();
-		} catch (IOException ex) {
-			System.err.println("Warning: IO error checking path: " + configFileName);
-			ConnectionUtil.configFileName = configFileName;
-		}
+		return getConnection(c.dbURL, c.dbDriverName, c.dbUserName, c.dbPassword);
 	}
 
 	/** Generate a Set<String> of the config names available
