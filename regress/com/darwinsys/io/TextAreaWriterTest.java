2678efec7dccd3c593fa1724da96996904f4b6c2
Ian Darwin <ian@darwinsys.com>
2007-01-16 17:32:56 +0000

    Close the writer that we opened.

--- regress/com/darwinsys/io/TextAreaWriterTest.java
+++ regress/com/darwinsys/io/TextAreaWriterTest.java
@@ -9,16 +9,17 @@ import junit.framework.TestCase;
 import com.darwinsys.io.TextAreaWriter;
 
 public class TextAreaWriterTest extends TestCase {
-	
+
 	private static final String HELLO_WORLD = "Hello World";
 
 	JTextArea ta = new JTextArea();
-	
+
 	public void testOne() {
 		PrintWriter x = new PrintWriter(new TextAreaWriter(ta));
 		x.print("Hello");
 		x.print(' ');
 		x.print("World");
+		x.close();
 		assertEquals(HELLO_WORLD, ta.getText());
 	}
 }
