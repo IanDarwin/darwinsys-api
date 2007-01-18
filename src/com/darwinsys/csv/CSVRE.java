483d4f1d7ce2ba937b804e4d1873c03231e249dd
Ian Darwin <ian@darwinsys.com>
2007-01-18 15:32:28 +0000

    Inline the trivial constructor. Make some variables final. Better variable names.

--- src/com/darwinsys/csv/CSVRE.java
+++ src/com/darwinsys/csv/CSVRE.java
@@ -28,29 +28,25 @@ public class CSVRE implements CSVParser {
 	 */
 	public static final String CSV_PATTERN =
 		"\"([^\"]+?)\",?|([^,]+),?|,";
-	private Pattern csvRE;
 
-	public static void main(String[] argv) throws IOException {
+	private final static Pattern csvRE = Pattern.compile(CSV_PATTERN);
+
+	public static void main(final String[] argv) throws IOException {
 		System.out.println(CSV_PATTERN);
 		new CSVRE().process(new BufferedReader(new InputStreamReader(System.in)));
 	}
 
-	/** Construct a regex-based CSV parser. */
-	public CSVRE() {
-		csvRE = Pattern.compile(CSV_PATTERN);
-	}
-
 	/** Process one file. Delegates to parse() a line at a time */
-	public void process(BufferedReader in) throws IOException {
+	public void process(final BufferedReader input) throws IOException {
 		String line;
 
 		// For each line...
-		while ((line = in.readLine()) != null) {
+		while ((line = input.readLine()) != null) {
 			System.out.println("line = `" + line + "'");
-			List l = parse(line);
-			System.out.println("Found " + l.size() + " items.");
-			for (int i = 0; i < l.size(); i++) {
-				System.out.print(l.get(i) + ",");
+			final List list = parse(line);
+			System.out.println("Found " + list.size() + " items.");
+			for (int i = 0; i < list.size(); i++) {
+				System.out.print(list.get(i) + ",");
 			}
 			System.out.println();
 		}
@@ -59,22 +55,24 @@ public class CSVRE implements CSVParser {
 	/** Parse one line.
 	 * @return List of Strings, minus their double quotes
 	 */
-	public List<String> parse(String line) {
-		List<String> list = new ArrayList<String>();
-		Matcher m = csvRE.matcher(line);
+	public List<String> parse(final String line) {
+		final List<String> list = new ArrayList<String>();
+		final Matcher m = csvRE.matcher(line);
 		// For each field
 		while (m.find()) {
 			String match = m.group();
-			if (match == null)
+			if (match == null) {
 				break;
+			}
 			if (match.endsWith(",")) {	// trim trailing ,
 				match = match.substring(0, match.length() - 1);
 			}
 			if (match.startsWith("\"")) { // assume also ends with
 				match = match.substring(1, match.length() - 1);
 			}
-			if (match.length() == 0)
-				match = null;
+			if (match.length() == 0) {
+				match = "";
+			}
 			list.add(match);
 		}
 		return list;
