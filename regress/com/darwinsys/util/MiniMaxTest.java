b4be730b7f239e2b79b06f90445a7261a2b364c9
Ian Darwin <ian@darwinsys.com>
2007-03-04 01:04:43 +0000

    New: simple test for MinMax stuff.

---
+++ regress/com/darwinsys/util/MiniMaxTest.java
@@ -0,0 +1,15 @@
+package com.darwinsys.util;
+
+import junit.framework.TestCase;
+
+public class MiniMaxTest extends TestCase {
+
+	public void testMaxInt() {
+		int[] data = { 1, 3, 4};
+		assertEquals(4, MiniMax.max(data));
+	}
+	public void testMaxInteger() {
+		Integer[] data = { 1, 3, 4};
+		assertEquals(new Integer(4), MiniMax.max(data));
+	}
+}
