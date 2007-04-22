40da9606845e110886b53b938eeb2449d434cefc
Ian Darwin <ian@darwinsys.com>
2007-04-22 23:47:05 +0000

    Add (String, Throwable) overload.

--- src/com/darwinsys/database/DataBaseException.java
+++ src/com/darwinsys/database/DataBaseException.java
@@ -1,16 +1,20 @@
 package com.darwinsys.database;
 
-/** Checked Exception DataBase Failures, so application code 
+/** Checked Exception DataBase Failures, so application code
  * outside of DAOs and the like does not need to know SQLException.
  */
 public class DataBaseException extends RuntimeException {
 
 	private static final long serialVersionUID = 3257572810388681269L;
-	
+
 	public DataBaseException() {
 		super();
 	}
 	public DataBaseException(String msg) {
 		super(msg);
+	}
+
+	public DataBaseException(String msg, Throwable e) {
+		super(msg, e);
 	}
 }
