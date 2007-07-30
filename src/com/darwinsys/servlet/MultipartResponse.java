07dd04c23b2227815aa00482585d455a956cda8a
Ian Darwin <ian@darwinsys.com>
2007-07-30 21:28:12 +0000

    Get output stream, fake up writer, as you can't really get both. Add usage example in doc comment.

--- src/com/darwinsys/servlet/MultipartResponse.java
+++ src/com/darwinsys/servlet/MultipartResponse.java
@@ -1,6 +1,7 @@
 package com.darwinsys.servlet;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServletResponse;
@@ -9,6 +10,22 @@ import javax.servlet.http.HttpServletResponse;
  * MultipartResponse lets a servlet generate "server push" responses,
  * such as simple SHORT animations (if too long you run the server
  * out of open files/sockets!).
+ * <p>Here is one usage example; another is in
+ * www.darwinsys.com/demo/MultipartResponseDemoServlet
+ * <pre>
+ * MultipartResponse mpr = new MultipartResponse(response);
+ * PrintWriter out = mpr.getWriter();
+
+ * mpr.startResponse("text/html");
+ * out.println("Searching...");
+ * mpr.endResponse();
+
+ * ... time passes
+
+ * mpr.startResponse("text/html");
+ * out.println("None found :-(");
+ * mpr.endResponse();
+ * </pre>
  * <p>
  * Can be used freely under the O'Reilly Copyright terms, see
  * http://www.oreilly.com/pub/a/oreilly/ask_tim/2001/codepolicy.html.
@@ -19,13 +36,15 @@ public class MultipartResponse {
 	private static final String BOUNDARY_TEXT = "End";
 	HttpServletResponse res;
 	PrintWriter out;
+	OutputStream outputStream;
 
 	boolean endedLastResponse = true;
 
 	public MultipartResponse(HttpServletResponse response) throws IOException {
 		// Save the response object and output stream
 		res = response;
-		out = res.getWriter();
+		outputStream = response.getOutputStream();
+		out = new PrintWriter(outputStream);
 
 		// Set things up
 		res.setContentType("multipart/x-mixed-replace;boundary=" +
@@ -35,6 +54,9 @@ public class MultipartResponse {
 	}
 
 	public void startResponse(String contentType) throws IOException {
+		if (out == null) {
+			throw new IllegalStateException("already called finish()");
+		}
 		// End the last response if necessary
 		if (!endedLastResponse) {
 			endResponse();
@@ -46,6 +68,9 @@ public class MultipartResponse {
 	}
 
 	public void endResponse() throws IOException {
+		if (out == null) {
+			throw new IllegalStateException("already called finish()");
+		}
 		// End the last response, and flush so the client sees the content
 		out.println();
 		out.println("--" + BOUNDARY_TEXT);
@@ -54,11 +79,21 @@ public class MultipartResponse {
 	}
 
 	public void finish() throws IOException {
+		if (out == null) {
+			throw new IllegalStateException("already called finish()");
+		}
 		out.println("--" + BOUNDARY_TEXT + "--");
 		out.flush();
+		// Not safe to re-use these after ending tag.
+		out = null;
+		outputStream = null;
 	}
 
 	public PrintWriter getWriter() {
 		return out;
+	}
+
+	public OutputStream getOutputStream() {
+		return outputStream;
 	}
 }
