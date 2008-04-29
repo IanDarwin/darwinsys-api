91c5de61910b8e93bcd5e927cb39925321319ad8
Ian Darwin <ian@darwinsys.com>
2008-04-29 15:25:27 +0000

    Add code to get Setter methods, as first step to building a Pojo-to-Dialog generator.

---
+++ regress/com/darwinsys/reflection/ReflectionUtilsTest.java
@@ -0,0 +1,40 @@
+package com.darwinsys.reflection;
+
+
+import java.lang.reflect.Method;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ReflectionUtilsTest {
+
+	ReflectionUtils target;
+	
+	@Before
+	public void setUp() throws Exception {
+		target = new ReflectionUtils();
+	}
+	
+	@Test
+	public void testGetSetters() throws Exception {
+		final List<Method> setters = 
+			target.getSetters("java.util.Date");
+		assertNotNull(
+			"getMethods returned a list", setters);
+		assertEquals(
+			"getMethods returned correct list", 7, setters.size());
+		
+		// The order is indeterminate (unless you want to
+		// rely on an implementation detail) so just make
+		// sure that setTime() is present...
+		boolean found = false;
+		for (Method m : setters) {
+			System.out.println(m);
+			if ("setTime".equals(m.getName()))
+				found = true;
+		}
+		assertTrue("found setTime()", found);
+	}
+}
---
+++ src/com/darwinsys/reflection/ReflectionUtils.java
@@ -0,0 +1,25 @@
+package com.darwinsys.reflection;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Get the Constructors and methods
+ * @author Ian F. Darwin, http://www.darwinsys.com/
+ * @version $Id$
+ */
+public class ReflectionUtils {
+
+	public List<Method> getSetters(String cName) throws ClassNotFoundException {
+		Class c = Class.forName(cName);
+		Method[] meths = c.getMethods();
+		List<Method> list = new ArrayList<Method>();
+		for (Method m : meths) {
+			if (m.getName().startsWith("set")) {
+				list.add(m);
+			}
+		}
+		return list;
+	}
+}
