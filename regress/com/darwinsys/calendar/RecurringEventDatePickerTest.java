1d4ec12a3dd0dd603d5476282bfa0c257e8f4215
Ian Darwin <ian@darwinsys.com>
2008-07-04 22:18:08 +0000

    New: Recurring Event Date Picker, from Derek.

---
+++ regress/com/darwinsys/calendar/RecurringEventDatePickerTest.java
@@ -0,0 +1,69 @@
+package com.darwinsys.calendar;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test RecurringEventDatePicker, using dates in May 2010.
+ * <pre>
+ * ian:31$ cal 5 2010
+ *       May 2010
+ * Su Mo Tu We Th Fr Sa
+ *                    1
+ *  2  3  4  5  6  7  8
+ *  9 10 11 12 13 14 15
+ * 16 17 18 19 20 21 22
+ * 23 24 25 26 27 28 29
+ * 30 31
+ * ian:32$
+ * </pre>
+ * @author Ian Darwin, http://www.darwinsys.com/ 
+ */
+public class RecurringEventDatePickerTest {
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testLowWeekOfMonth() {
+		new RecurringEventDatePicker(0, Calendar.SATURDAY);
+	}
+	@Test(expected=IllegalArgumentException.class)
+	public void testHighWeekOfMonth() {
+		new RecurringEventDatePicker(5, Calendar.SATURDAY);
+	}
+	@Test(expected=IllegalArgumentException.class)
+	public void testLowDayOfWeek() {
+		new RecurringEventDatePicker(0, 0);
+	}
+	@Test(expected=IllegalArgumentException.class)
+	public void testHighDayOfWeek() {
+		new RecurringEventDatePicker(0, 7);
+	}
+	
+	@Test 
+	public void testFirstSunday() {
+		RecurringEventDatePicker mp = new RecurringEventDatePicker(1, Calendar.SUNDAY);
+		mp.today = makeDate(2010, 05, 01);
+		Calendar c = mp.getMeetingDate(0);
+		assertEquals(2010, c.get(Calendar.YEAR));
+		assertEquals(Calendar.MAY, c.get(Calendar.MONTH));
+		assertEquals(2, c.get(Calendar.DAY_OF_MONTH));
+	}
+	
+	@Test 
+	public void testThirdMonday() {
+		RecurringEventDatePicker mp = new RecurringEventDatePicker(3, Calendar.MONDAY);
+		mp.today = makeDate(2010, 05, 05);
+		Calendar c = mp.getMeetingDate(0);
+		assertEquals(2010, c.get(Calendar.YEAR));
+		assertEquals(Calendar.MAY, c.get(Calendar.MONTH));
+		assertEquals(17, c.get(Calendar.DAY_OF_MONTH));
+	}
+	
+	@SuppressWarnings("deprecation")
+	private Date makeDate(int y, int m, int d) {
+		return new Date(y-1900, m - 1, d);
+	}
+}
---
+++ src/com/darwinsys/calendar/RecurringEventDatePicker.java
@@ -0,0 +1,72 @@
+package com.darwinsys.calendar;
+
+import java.util.Date;
+import java.util.Calendar;
+
+/**
+ * Pick the date that a recurring event will fall on, e.g.,
+ * gtabug.ca currently meets on the Third Wednesday of each month:
+ * <pre>
+ * RecurringEventDatePicker mp = new RecurringEventDatePicker(3, Calendar.WEDNESDAY);
+ * Calendar nextMeeting = mp.getNextMeeting(0); // next
+ * </pre>
+ * In a JSP you might use:
+ * <pre>
+ * RecurringEventDatePicker mp = new RecurringEventDatePicker(3, Calendar.WEDNESDAY);
+ * DateFormat dfm = new SimpleDateFormat("MMMM dd, yyyy");
+ * out.println("<li>" + dfm.format(mp.GetMeetingDate(0));
+ * out.println("<li>" + dfm.format(mp.GetMeetingDate(1));
+ * out.println("<li>" + dfm.format(mp.GetMeetingDate(2)); 
+ * </pre>
+ * @author Original code by Derek.
+ * @author Improvements and JUnit tests by Ian Darwin
+ */
+public class RecurringEventDatePicker {
+	
+	private int dayOfWeek = Calendar.WEDNESDAY;
+	
+	private int weekOfMonth = 3;
+	
+	/** package protected only for testing, not for general use */
+	Date today = new Date();
+
+	/**
+	 * Construct a RecurringEventDatePicker
+	 * @param weekOfMonth
+	 * @param dayOfWeek
+	 */
+	public RecurringEventDatePicker(int weekOfMonth, int dayOfWeek) {
+		super();
+		if (weekOfMonth < 1 || weekOfMonth > 5) {
+			throw new IllegalArgumentException("weekOfMonth must be in 1..5");
+		}
+		if (dayOfWeek < 0 || dayOfWeek > 6) {
+			throw new IllegalArgumentException("weekOfMonth must be in 0..6");
+		}
+		this.weekOfMonth = weekOfMonth;
+		this.dayOfWeek = dayOfWeek;
+	}
+
+	public Calendar getMeetingDate(int meetingsAway) {
+		
+		// start from today
+		Calendar thisMeeting = Calendar.getInstance();
+		thisMeeting.setTime(today);
+		thisMeeting.set(Calendar.DAY_OF_WEEK, dayOfWeek);
+		thisMeeting.set(Calendar.DAY_OF_WEEK_IN_MONTH, weekOfMonth);		
+		
+		// has the meeting already happened this month
+		if (thisMeeting.getTime().compareTo(today) < 0 ) {
+			// start from next month
+			meetingsAway++;
+		}
+		
+		if (meetingsAway > 0) {
+			thisMeeting.add(Calendar.MONTH, meetingsAway);
+			thisMeeting.set(Calendar.DAY_OF_WEEK, dayOfWeek);
+			thisMeeting.set(Calendar.DAY_OF_WEEK_IN_MONTH, weekOfMonth);
+		}
+		
+		return thisMeeting;
+	}
+}
