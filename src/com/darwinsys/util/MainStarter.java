093b9181427e8ff2d0942e2430761f7db7009a5d
Ian Darwin <ian@darwinsys.com>
2009-01-26 01:26:16 +0000

    Put in a SecurityManager to stop one Task from doing System.exit and killing the whole shebang.

--- src/com/darwinsys/util/MainStarter.java
+++ src/com/darwinsys/util/MainStarter.java
@@ -1,6 +1,7 @@
 package com.darwinsys.util;
 
 import java.lang.reflect.Method;
+import java.security.Permission;
 
 /** Start a bunch of Main programs, to avoid starting a JVM for each
  * XXX TODO a taskbar icon with a menu to exit, and a SecurityManager
@@ -12,10 +13,10 @@ public class MainStarter {
 			System.err.println("Usage: MainStarter class class [...]");
 		}
 		Class[] typeParams = { args.getClass() };
-		
+
 		for (final String clazz : args) {
 			try {
-				final Class c = Class.forName(clazz);
+				final Class<?> c = Class.forName(clazz);
 				final Method m = c.getMethod("main", typeParams);
 				final String[] noArgs = new String[0];
 				final Object[] passedArgs = { noArgs };
@@ -25,13 +26,35 @@ public class MainStarter {
 							m.invoke(null, passedArgs);
 						} catch (Throwable t) {
 							System.out.println(clazz + " failed: " + t);
+							t.printStackTrace();
 						}
 					}
 				}).start();
 
 			} catch (Throwable t) {
 				System.out.println(clazz + " failed: " + t);
+				t.printStackTrace();
 			}
 		}
+
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// stupid exception
+		}
+
+		// Now the tasks have had time to start, put in a security manager to stop them from exiting.
+		// Since this is a main application, allow everything else (for now).
+		System.setSecurityManager(new SecurityManager() {
+			public void checkPermission(Permission p) {
+				// empty
+			}
+			public void checkPermission(Permission p, Object secContext) {
+				// empty
+			}
+			public void checkExit(int status) {
+				throw new SecurityException("One started task cannot exit this whole mess");
+			}
+		});
 	}
 }
