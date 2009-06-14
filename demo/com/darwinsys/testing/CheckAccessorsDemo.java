21416725de5f1b1caf7e578ad490b0b7484a9f80
Ian Darwin <ian@darwinsys.com>
2009-06-14 17:49:11 +0000

    Don't hard-code Calendar implementation class.

--- demo/com/darwinsys/testing/CheckAccessorsDemo.java
+++ demo/com/darwinsys/testing/CheckAccessorsDemo.java
@@ -1,10 +1,10 @@
 package com.darwinsys.testing;
 
-import java.util.GregorianCalendar;
+import java.util.Calendar;
 
 public class CheckAccessorsDemo {
 	public static void main(String[] args) throws Exception {
-		Class<?> c = GregorianCalendar.class;
+		Class<?> c = Calendar.getInstance().getClass();
 		System.out.printf("Testing the %s class' accessors%n", c);
 		CheckAccessors.process(c);
 	}
