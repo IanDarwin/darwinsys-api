771c80de25483e4d7c4a9f08af262d85e66ebf55
Ian Darwin <ian@darwinsys.com>
2010-03-21 01:29:24 +0000

    The ever-popular language enum.

---
+++ src/com/darwinsys/geo/Language.java
@@ -0,0 +1,24 @@
+package lhb/model;
+
+public enum Language {
+	en("English"),
+	cn_tw("Chinese(Cantonese)"),
+	cn_zh("Chinese(Mandarin)"),
+	de("German"),
+	es("Spanish"),
+	fr("French"),
+	jp("Japanese"),
+	kr("Korean"),
+	pl("Polish"),
+	ru("Russian"),
+
+	private final String name;
+
+	private Language(String name) {
+		this.name = name;
+	}
+
+	@Override public String toString() {
+		return name;
+	}
+}
