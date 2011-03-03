0689251ba46dacb0e9dc42d6876edc856f2997e9
Ian Darwin <ian@darwinsys.com>
2011-03-03 01:49:04 +0000

    List of Provinces and States (etc.).

---
+++ src/com/darwinsys/geo/CanadaProvince.java
@@ -0,0 +1,36 @@
+package com.darwinsys.geo;
+
+/**
+ * Canadian provinces
+ */
+public enum CanadaProvince {
+	AB("Alberta"),
+	BC("British Columbia"),
+	MB("Manitoba"),
+	NB("New Brunswick"),
+	NL("Newfoundland & Labrador"),
+	NS("Nova Scotia"),
+	NV("Nunavut"),
+	NW("North West Territories"),
+	ON("Ontario"),
+	PE("Prince Edward Islane"),
+	QC("Quebec"),
+	SK("Saskatchewan"),
+	YK("Yukon Territories"),
+	;
+	
+	private final String longName;
+
+	private CanadaProvince(String longName) {
+		this.longName = longName;
+	}
+
+	public String getLongName() {
+		return longName;
+	}
+	
+	@Override
+	public String toString () {
+		return this.getLongName();
+	}
+}
---
+++ src/com/darwinsys/geo/USState.java
@@ -0,0 +1,88 @@
+package com.darwinsys.geo;
+
+/**
+ * US States provinces
+ */
+public enum USState {
+	AL("ALABAMA"),
+	AK("ALASKA"),
+	AS("AMERICAN SAMOA"),
+	AZ("ARIZONA"),
+	AR("ARKANSAS"),
+	CA("CALIFORNIA"),
+	CO("COLORADO"),
+	CT("CONNECTICUT"),
+	DE("DELAWARE"),
+	DC("DISTRICT OF COLUMBIA"),
+	FM("FEDERATED STATES OF MICRONESIA"),
+	FL("FLORIDA"),
+	GA("GEORGIA"),
+	GU("GUAM"),
+	HI("HAWAII"),
+	ID("IDAHO"),
+	IL("ILLINOIS"),
+	IN("INDIANA"),
+	IA("IOWA"),
+	KS("KANSAS"),
+	KY("KENTUCKY"),
+	LA("LOUISIANA"),
+	ME("MAINE"),
+	MH("MARSHALL ISLANDS"),
+	MD("MARYLAND"),
+	MA("MASSACHUSETTS"),
+	MI("MICHIGAN"),
+	MN("MINNESOTA"),
+	MS("MISSISSIPPI"),
+	MO("MISSOURI"),
+	MT("MONTANA"),
+	NE("NEBRASKA"),
+	NV("NEVADA"),
+	NH("NEW HAMPSHIRE"),
+	NJ("NEW JERSEY"),
+	NM("NEW MEXICO"),
+	NY("NEW YORK"),
+	NC("NORTH CAROLINA"),
+	ND("NORTH DAKOTA"),
+	MP("NORTHERN MARIANA ISLANDS"),
+	OH("OHIO"),
+	OK("OKLAHOMA"),
+	OR("OREGON"),
+	PW("PALAU"),
+	PA("PENNSYLVANIA"),
+	PR("PUERTO RICO"),
+	RI("RHODE ISLAND"),
+	SC("SOUTH CAROLINA"),
+	SD("SOUTH DAKOTA"),
+	TN("TENNESSEE"),
+	TX("TEXAS"),
+	UT("UTAH"),
+	VT("VERMONT"),
+	VI("VIRGIN ISLANDS"),
+	VA("VIRGINIA"),
+	WA("WASHINGTON"),
+	WV("WEST VIRGINIA"),
+	WI("WISCONSIN"),
+	WY("WYOMING"),
+	AE("Armed Forces Africa"),
+	AA("Armed Forces Americas"),
+	AE("Armed Forces Canada"),
+	AE("Armed Forces Europe"),
+	AE("Armed Forces Middle East"),
+	AP("Armed Forces Pacific"),
+	;
+	
+	private final String longName;
+
+	private USState(String longName) {
+		this.longName = longName;
+	}
+
+	public String getLongName() {
+		return longName;
+	}
+	
+	@Override
+	public String toString () {
+		return this.getLongName();
+	}
+}
