fea2629bea3712681cf4ce5a8b8cd1b7e577584d
Ian Darwin <ian@darwinsys.com>
2011-03-08 17:49:06 +0000

    Provide three overloads of converse() with more and fewer arguments. The longer ones accept a username and password.

--- src/com/darwinsys/net/ConversationURL.java
+++ src/com/darwinsys/net/ConversationURL.java
@@ -7,14 +7,35 @@ import java.io.PrintWriter;
 import java.net.URL;
 import java.net.URLConnection;
 
+import org.apache.commons.codec.binary.Base64;
+
 /**
  * Read a URL's data from a URLConnection
  */
 public class ConversationURL {
+	
+	/** Run a conversation and return the result, with no
+	 * postBogy and not credentials.
+	 * @param host The web service site address
+	 * @param port the port number (usually 80)
+	 * @param path The servlet or component path
+	 * @return
+	 */
+	 public static String converse(String host, int port, String path) {
+		 return converse(host, port, path, null, null, null);
+	 }
+	
+	 public static String converse(String host, int port, String path, String postBody, String userName, String password) throws IOException {
+		 URL url = new URL("http", host, 80, path);
+		 return converse(url, postBody, userName, password);
+	 }
 	    
-	    public static String converse(String host, int port, String path, String postBody) throws IOException {
-			URL url = new URL("http", host, 80, path);
-	        URLConnection conn = url.openConnection();
+	 public static String converse(URL url, String postBody, String userName, String password) throws IOException {
+		 URLConnection conn = url.openConnection();
+		 if (userName != null || password != null) {
+	        String auth = Base64.encodeBase64(userName + ":" + password);
+	        conn.setRequestProperty("Authorization", auth);
+		 }
 	        boolean post = postBody != null;
 	        if (post)
 	        	conn.setDoInput(true);
