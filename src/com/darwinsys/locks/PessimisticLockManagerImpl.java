e032fc1b37fd387e380b13585a57652b5af44fb7
Ian Darwin <ian@darwinsys.com>
2011-07-11 20:24:48 +0000

    Close() releases and frees all locks. tryLock() code clarified a bit. Doc comment tweaks.

--- src/com/darwinsys/locks/PessimisticLockManagerImpl.java
+++ src/com/darwinsys/locks/PessimisticLockManagerImpl.java
@@ -48,12 +48,18 @@ public class PessimisticLockManagerImpl<T> implements PessimisticLockManager<T> 
 		}
 	}
 	
-	/** Try to get the lock for the given ID */
+	/** Try to get the lock for the given ID
+	 * @param id The primary key object, which must implement equals+hashCode correctly
+	 */
 	public Lock tryLock(T id) {
 		 return tryLock(id, null);
 	}
 	
-	/** Try to get the lock for the given ID, with reportable owner */
+	/** Try to get the lock for the given ID, with reportable owner
+	 * @param id The primary key object, which must implement equals+hashCode correctly
+	 * @param owner An arbitrary object for reporting, but often the customer or user on 
+	 * whose behalf the app is locking the id.
+	 */
 	public synchronized Lock tryLock(T id, Object owner) {
 		if (lockReaper == null) {
 			start();
@@ -72,18 +78,23 @@ public class PessimisticLockManagerImpl<T> implements PessimisticLockManager<T> 
 	 */
 	public synchronized boolean releaseLock(Lock lock) {
 		if (locks.containsKey(lock)) {
-			locks.remove(lock);
 			((LockImpl)lock).setReleased(true);
-			return true;
+			final boolean removed = locks.remove(lock) != null;
+			return removed;
 		}
 		return false;
 	}
 
 	/**
-	 *  Shutdown this LockManager; just tells its LockReaper to go away.
+	 *  Shutdown this LockManager; tells its LockReaper to go away
+	 *  and releases and discards all locks.
 	 */
-	public void close() {
+	public synchronized void close() {
 		lockReaper.setDone(true);
+		for (Lock l : getLocks()) {
+			l.release();
+			locks.remove(l);
+		}
 	}
 
 	public int getTimeout() {
