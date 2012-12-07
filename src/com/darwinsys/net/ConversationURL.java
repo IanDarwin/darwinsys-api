8e5896e829d002d2f6b2e8740d0bfa02325ff9af
Ian Darwin <ian@darwinsys.com>
2012-12-07 15:31:30 +0000

    Add a converseStream() method, returns the InputStream for feeding into e.g., an XML parser, instead of stringing and then un-stringing.

--- src/com/darwinsys/net/ConversationURL.java
+++ src/com/darwinsys/net/ConversationURL.java
@@ -2,6 +2,7 @@ package com.darwinsys.net;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.URL;
@@ -42,8 +43,8 @@ public class ConversationURL {
 		 return converse(url, postBody, userName, password);
 	 }
 	
-	 /** Run a conversation and return the result, with
-	  * the given postBody and no credentials.
+	 /** Run a conversation and return the result as a (long) String, with inputs
+	  * the given postBody and credentials.
 	  * @param url The URL for the restful web service
 	  * @param postBody The body to send (null == use GET)
 	  * @param userName Credentials
@@ -52,6 +53,31 @@ public class ConversationURL {
 	  * @throws IOException 
 	  */
 	 public static String converse(URL url, String postBody, String userName, String password) throws IOException {
+		 InputStream converseStream = converseStream(url, postBody, userName, password);
+
+		StringBuilder sb = new StringBuilder();
+		BufferedReader in = new BufferedReader(
+				new InputStreamReader(converseStream));
+		String line;
+
+		while ((line = in.readLine()) != null) {
+			sb.append(line);
+		}
+
+		in.close();
+		return sb.toString();
+	}
+	
+	 /** Run a conversation and return the resulting InputStream, with inputs
+	  * the given postBody and credentials.
+	  * @param url The URL for the restful web service
+	  * @param postBody The body to send (null == use GET)
+	  * @param userName Credentials
+	  * @param password Credentials
+	  * @return The result of the conversation.
+	  * @throws IOException 
+	  */
+	 public static InputStream converseStream(URL url, String postBody, String userName, String password) throws IOException {
 		URLConnection conn = url.openConnection();
 		if (userName != null || password != null) {
 			String cred = userName + ":" + password;
@@ -72,19 +98,7 @@ public class ConversationURL {
 			out.println(postBody);
 			out.close();
 		}
-
-		StringBuilder sb = new StringBuilder();
-		BufferedReader in = new BufferedReader(
-				new InputStreamReader(conn.getInputStream()));
-		String line;
-
-		while ((line = in.readLine()) != null) {
-			sb.append(line);
-		}
-
-		in.close();
-		return sb.toString();
-	}
-	
-
+		return conn.getInputStream();
+	 }
+		
 }
