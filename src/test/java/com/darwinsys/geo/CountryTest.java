c73d3c7de0d8ddb069ed1276bf7302e4dcf2d221
Ian Darwin <ian@darwinsys.com>
2013-11-27 16:24:54 -0500

    More tests, and, minor repairs, some as a result.

---
+++ src/test/java/com/darwinsys/geo/CountryTest.java
@@ -0,0 +1,24 @@
+package com.darwinsys.geo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CountryTest {
+
+	@Test
+	public void testGetLongName() {
+		fail("not implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsCanadaOrUSA() {
+		fail("Not yet implemented");
+	}
+
+}
