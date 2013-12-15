3535aee90a414ac4f4fcd6c48b6a507a7e0d6c9e
Ian Darwin <ian@darwinsys.com>
2013-12-14 22:49:32 -0500

    Restored from book mss

---
+++ src/demo/java/com/darwinsys/io/FileIoDemo.java
@@ -0,0 +1,18 @@
+package io;
+
+import com.darwinsys.util.FileIO;
+
+import java.io.*;
+
+public class FileIODemo {
+    public static void main(String[] av) {
+        try {
+            FileIO.copyFile("FileIO.java", "FileIO.bak");
+            FileIO.copyFile("FileIO.class", "FileIO-class.bak");
+        } catch (FileNotFoundException e) {
+            System.err.println(e);
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+}
