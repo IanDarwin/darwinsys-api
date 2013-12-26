1a9afe0e591a587be580aadd8fd9f3f080e8cfa8
IanDarwin <foss@darwinsys.com>
2013-12-26 15:35:52 -0500

    Undead.

---
+++ src/demo/java/com/darwinsys/swingui/WindowCloserDemo.java
@@ -0,0 +1,20 @@
+package com.darwinsys.swingui;
+
+import java.awt.Frame;
+import java.awt.Label;
+import com.darwinsys.swingui.WindowCloser;
+
+/* Example of closing a Window. */
+// BEGIN main
+public class WindowCloserDemo {
+
+    /* Main method */
+    public static void main(String[] argv) {
+        Frame f = new Frame("Close Me");
+        f.add(new Label("Try Titlebar Close", Label.CENTER));
+        f.setSize(100, 100);
+        f.setVisible(true);
+        f.addWindowListener(new WindowCloser(f, true));
+    }
+}
+// END main
