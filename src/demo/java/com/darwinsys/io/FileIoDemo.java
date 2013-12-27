bca537f0fa07912f9a4f2bc84cf522650cb01686
Ian Darwin <foss@darwinsys.com>
2013-12-26 21:06:27 -0500

    Simplify ehandling; add BEGIN/END marks.

--- src/demo/java/com/darwinsys/io/FileIoDemo.java
+++ src/demo/java/com/darwinsys/io/FileIoDemo.java
@@ -1,17 +1,16 @@
 package com.darwinsys.io;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
+// BEGIN main
 public class FileIoDemo {
     public static void main(String[] av) {
         try {
             FileIO.copyFile("FileIO.java", "FileIO.bak");
             FileIO.copyFile("FileIO.class", "FileIO-class.bak");
-        } catch (FileNotFoundException e) {
-            System.err.println(e);
         } catch (IOException e) {
             System.err.println(e);
         }
     }
 }
+// END main
