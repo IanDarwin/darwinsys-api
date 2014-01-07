f14f667aaa537c6fbe58ff4ef156311af7217988
Ian Darwin <foss@darwinsys.com>
2014-01-07 08:50:33 -0500

    Import police

--- src/main/java/com/darwinsys/installers/InstallerUnpacker.java
+++ src/main/java/com/darwinsys/installers/InstallerUnpacker.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.InvocationTargetException;
 import java.util.Date;
 import java.util.Enumeration;
 import java.util.SortedSet;
