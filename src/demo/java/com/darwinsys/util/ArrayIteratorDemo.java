92f6d63780512fe7e60be163d017cd529d53971c
Ian Darwin <foss@darwinsys.com>
2014-01-25 18:48:37 -0500

    Indentation.

--- src/demo/java/com/darwinsys/util/ArrayIteratorDemo.java
+++ src/demo/java/com/darwinsys/util/ArrayIteratorDemo.java
@@ -2,10 +2,12 @@
 package com.darwinsys.util;
 
 public class ArrayIteratorDemo {
-		private final static String[] names = {
-			"rose", "petunia", "tulip"
-		};
-		public static void main(String[] args) {
+	
+	private final static String[] names = {
+		"rose", "petunia", "tulip"
+	};
+	
+	public static void main(String[] args) {
 		ArrayIterator<String> arrayIterator = new ArrayIterator<>(names);
 		for (String s : arrayIterator) {
 			System.out.println(s);
