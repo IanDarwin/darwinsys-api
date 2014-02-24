920c92133b712b0afffffc2654d77c3efa0b1e10
Ian Darwin <foss@darwinsys.com>
2014-02-23 21:35:02 -0500

    Found.

--- src/test/java/com/darwinsys/csv/CSVSimple.java
+++ src/test/java/com/darwinsys/csv/CSVSimple.java
@@ -1,27 +1,24 @@
 // BEGIN main
 package com.darwinsys.csv;
 
-import java.util.Iterator;
 import java.util.List;
 
 /* Simple demo of CSV parser class. */
 public class CSVSimple {
 	public static void main(String[] args) {
 		CSVImport parser = new CSVImport();
-		List list = parser.parse(
+		List<String> list = parser.parse(
 			"\"LU\",86.25,\"11/4/1998\",\"2:19PM\",+4.0625");
-		Iterator it = list.iterator();
-		while (it.hasNext()) {
-			System.out.println(it.next());
+		for (String word : list) {
+			System.out.println(word);
 		}
 
 		// Now try with a non-default separator
 		parser = new CSVImport('|');
 		list = parser.parse(
 			"\"LU\"|86.25|\"11/4/1998\"|\"2:19PM\"|+4.0625");
-		it = list.iterator();
-		while (it.hasNext()) {
-			System.out.println(it.next());
+		for (String word : list) {
+			System.out.println(word);
 		}
 	}
 }
