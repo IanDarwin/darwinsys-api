94aa4a92b7bfae2f4b88c156027fa54aba8ac6fd
Ian Darwin <ian@darwinsys.com>
2015-03-12 20:44:23 -0400

    Formatting Language (Wiki, AsciiDoc, etc.) enum

---
+++ src/main/java/com/darwinsys/formatting/FormatLanguage.java
@@ -0,0 +1,15 @@
+package com.darwinsys.formatting;
+
+/**
+ * The language that an entry (such as text in a Blog or a Cookbook Site)
+ * has been encoded in during authoring.
+ * This explicitly does not define what the author typed, but what was stored
+ * as a result (e.g., consider the various JavaScript edit widgets where you type
+ * in text and use mouse editing, but the content is sadly stored in HTML).
+ */
+public enum FormatLanguage {
+	PLAIN,
+	HTML,
+	ASCIIDOC,
+	WIKI
+}
