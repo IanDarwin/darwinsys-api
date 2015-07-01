64c32029c2e9b6c317d12dd5fde2ad89bd11fcd1
Ian Darwin <ian@darwinsys.com>
2015-07-01 11:33:41 -0400

    New version 1.1.0 requires Java 8, uses java.time in RecurringEventDatePicker.
    REDP API changes to require Java 8 args in constructor; a legacy API is provided
    for getting the event dates however.

--- src/main/java/com/darwinsys/calendar/RecurringEventDatePicker.java
+++ src/main/java/com/darwinsys/calendar/RecurringEventDatePicker.java
@@ -1,79 +1,119 @@
 package com.darwinsys.calendar;
 
-import java.util.Date;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 /**
  * Pick the date that a recurring event will fall on, e.g.,
  * gtabug.ca currently meets on the Third Wednesday of each month:
  * <pre>
- * RecurringEventDatePicker mp = new RecurringEventDatePicker(3, Calendar.WEDNESDAY);
- * Calendar nextMeeting = mp.getNextMeeting(0); // next
+ * RecurringEventDatePicker mp = new RecurringEventDatePicker(3, DayOfWeek.WEDNESDAY);
+ * LocalDate nextMeeting = mp.getNextMeeting(0); // next
  * </pre>
  * In a JSP you might use:
  * <pre>
- * RecurringEventDatePicker mp = new RecurringEventDatePicker(3, Calendar.WEDNESDAY);
- * DateFormat dfm = new SimpleDateFormat("MMMM dd, yyyy");
- * out.println("*" + dfm.format(mp.getEventDate(0));
- * out.println("*" + dfm.format(mp.getEventDate(1));
- * out.println("*" + dfm.format(mp.getEventDate(2)); 
+ * Next three meetings: <%
+ * RecurringEventDatePicker dp = new RecurringEventDatePicker(3, DayOfWeek.WEDNESDAY);
+ * DateTimeFormatter dfm = 
+ * 			DateTimeFormatter.ofPattern("MMMM d, yyyy");
+ * out.println("*" + dp.getEventDate(0).format(dfm));
+ * out.println("*" + dp.getEventDate(1).format(dfm));
+ * out.println("*" + dp.getEventDate(2).format(dfm)); %>
  * </pre>
- * XXX TODO Revise for Java 8 date/time API.
  * @author Original code by Derek Marcotte.
  * @author Improvements and JUnit tests by Ian Darwin
+ * @author Rewritten for Java 8 date/time API by Ian Darwin
  */
 public class RecurringEventDatePicker {
 	
-	private int dayOfWeek = Calendar.WEDNESDAY;
+	private DayOfWeek dayOfWeek;
 	
 	private int weekOfMonth = 3;
 	
-	/** package protected only for testing, not for general use */
-	Date today = new Date();
+	private LocalTime hourOfDay;
+	
+	// non-private only for testing!
+	LocalDate now = LocalDate.now();
 
 	/**
-	 * Construct a RecurringEventDatePicker
+	 * Construct a RecurringEventDatePicker for e.g, the 
+	 * third Tuesday of every month:
+	 * new RecurringEventDatePicker(3, DayOfWeek.TUESDAY);
 	 * @param weekOfMonth The recurrence week of the month
 	 * @param dayOfWeek The recurrence day of the week
 	 */
-	public RecurringEventDatePicker(int weekOfMonth, int dayOfWeek) {
+	public RecurringEventDatePicker(int weekOfMonth, DayOfWeek dayOfWeek, LocalTime hourOfDay) {
 		super();
 		if (weekOfMonth < 1 || weekOfMonth > 5) {
 			throw new IllegalArgumentException("weekOfMonth must be in 1..5");
 		}
-		if (dayOfWeek < 0 || dayOfWeek > 6) {
-			throw new IllegalArgumentException("weekOfMonth must be in 0..6");
-		}
 		this.weekOfMonth = weekOfMonth;
 		this.dayOfWeek = dayOfWeek;
+		this.hourOfDay = hourOfDay;
+	}
+	
+	public RecurringEventDatePicker(int weekOfMonth, DayOfWeek dayOfWeek) {
+		this(weekOfMonth, dayOfWeek, LocalTime.of(12, 00));
 	}
 
 	/**
-	 * Get the event date for the 'n'th next meeting
+	 * Get a LEGACY java.util.Calendar representing
+	 * the event date for the 'n'th next meeting
 	 * @param meetingsAway the number of meetings into the future that
 	 * you're interested in.
 	 * @return The given date
 	 */
+	@Deprecated
 	public Calendar getEventDate(int meetingsAway) {
 		
-		// start from today
-		Calendar thisMeeting = Calendar.getInstance();
-		thisMeeting.setTime(today);
-		thisMeeting.set(Calendar.DAY_OF_WEEK, dayOfWeek);
-		thisMeeting.set(Calendar.DAY_OF_WEEK_IN_MONTH, weekOfMonth);		
+		LocalDate when = getEventLocalDate(meetingsAway);
+		LocalDateTime zhen = LocalDateTime.of(when, hourOfDay);
+		ZonedDateTime zdt = ZonedDateTime.of(zhen, ZoneId.systemDefault());
 		
+		return GregorianCalendar.from(zdt);
+	}
+	
+	/**
+	 * Get a java.time.LocalDate representing
+	 * the event date for the 'n'th next meeting
+	 * for this RecurringEventDatePicker
+	 * @param meetingsAway the number of meetings into the future that
+	 * you're interested in.
+	 * @return The given date
+	 */
+	public LocalDate getEventLocalDate(int meetingsAway) {
+
+		LocalDate thisMeeting = meetingForMonth(now);
 		// has the meeting already happened this month
-		if (thisMeeting.getTime().compareTo(today) < 0 ) {
+		if (thisMeeting.isBefore(now)) {
 			// start from next month
 			meetingsAway++;
 		}
-		
 		if (meetingsAway > 0) {
-			thisMeeting.add(Calendar.MONTH, meetingsAway);
-			thisMeeting.set(Calendar.DAY_OF_WEEK, dayOfWeek);
-			thisMeeting.set(Calendar.DAY_OF_WEEK_IN_MONTH, weekOfMonth);
+			thisMeeting = meetingForMonth(
+				thisMeeting.plusMonths(meetingsAway));
 		}
 		
 		return thisMeeting;
+	}
+	
+	/**
+	 * Get the meeting in the month of the given LocalDate
+	 * for this RecurringDatePicker
+	 * @param dateWithMonth A date in the same month as the meeting
+	 * @return The date of the meeting
+	 */
+	public LocalDate meetingForMonth(LocalDate dateWithMonth) {
+		return
+			dateWithMonth.with(TemporalAdjusters.firstInMonth(dayOfWeek))
+				.plusWeeks(Math.max(0, weekOfMonth - 1));
 	}
 }
--- src/test/java/com/darwinsys/calendar/RecurringEventDatePickerTest.java
+++ src/test/java/com/darwinsys/calendar/RecurringEventDatePickerTest.java
@@ -1,16 +1,17 @@
 package com.darwinsys.calendar;
 
+import static org.junit.Assert.assertEquals;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.util.Calendar;
-import java.util.Date;
 
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 /**
  * Test RecurringEventDatePicker, using dates in May 2010.
  * <pre>
- * ian:31$ cal 5 2010
+ * $ cal 5 2010
  *       May 2010
  * Su Mo Tu We Th Fr Sa
  *                    1
@@ -19,51 +20,67 @@ import static org.junit.Assert.*;
  * 16 17 18 19 20 21 22
  * 23 24 25 26 27 28 29
  * 30 31
- * ian:32$
+ * $ cal 6 2010
+ *      June 2010
+ * Su Mo Tu We Th Fr Sa
+ *        1  2  3  4  5
+ *  6  7  8  9 10 11 12
+ * 13 14 15 16 17 18 19
+ * 20 21 22 23 24 25 26
+ * 27 28 29 30
  * </pre>
  * @author Ian Darwin, http://www.darwinsys.com/ 
  */
 public class RecurringEventDatePickerTest {
 	
+	// Test constructor error handling first
 	@Test(expected=IllegalArgumentException.class)
 	public void testLowWeekOfMonth() {
-		new RecurringEventDatePicker(0, Calendar.SATURDAY);
+		new RecurringEventDatePicker(0, DayOfWeek.SATURDAY);
 	}
 	@Test(expected=IllegalArgumentException.class)
 	public void testHighWeekOfMonth() {
-		new RecurringEventDatePicker(5, Calendar.SATURDAY);
-	}
-	@Test(expected=IllegalArgumentException.class)
-	public void testLowDayOfWeek() {
-		new RecurringEventDatePicker(0, 0);
-	}
-	@Test(expected=IllegalArgumentException.class)
-	public void testHighDayOfWeek() {
-		new RecurringEventDatePicker(0, 7);
+		new RecurringEventDatePicker(6, DayOfWeek.SATURDAY);
 	}
 	
 	@Test 
 	public void testFirstSunday() {
-		RecurringEventDatePicker mp = new RecurringEventDatePicker(1, Calendar.SUNDAY);
-		mp.today = makeDate(2010, 05, 01);
-		Calendar c = mp.getEventDate(0);
-		assertEquals(2010, c.get(Calendar.YEAR));
-		assertEquals(Calendar.MAY, c.get(Calendar.MONTH));
-		assertEquals(2, c.get(Calendar.DAY_OF_MONTH));
+		RecurringEventDatePicker mp = 
+			new RecurringEventDatePicker(1, DayOfWeek.SUNDAY);
+		mp.now = LocalDate.of(2010, 05, 01);
+		LocalDate c = mp.getEventLocalDate(0);
+		System.out.printf("%s => %s%n", mp.now, c);
+		assertEquals(LocalDate.of(2010,5,2), c);
 	}
 	
 	@Test 
 	public void testThirdMonday() {
-		RecurringEventDatePicker mp = new RecurringEventDatePicker(3, Calendar.MONDAY);
-		mp.today = makeDate(2010, 05, 05);
+		RecurringEventDatePicker mp = 
+			new RecurringEventDatePicker(3, DayOfWeek.MONDAY);
+		mp.now = LocalDate.of(2010, 05, 05);
+		LocalDate c = mp.getEventLocalDate(0);
+		assertEquals(LocalDate.of(2010, 5, 17), c);
+		LocalDate d = mp.getEventLocalDate(1);
+		assertEquals(LocalDate.of(2010, 6, 21), d);
+	}
+	
+	@Test
+	public void testMeetingForMonth() {
+		RecurringEventDatePicker mp = 
+				new RecurringEventDatePicker(1, DayOfWeek.SUNDAY);
+		LocalDate now = LocalDate.of(2010, 05, 22);
+		assertEquals(LocalDate.of(2010, 05, 02), mp.meetingForMonth(now));
+	}
+	
+	@Test
+	public void testLegacy() {
+		RecurringEventDatePicker mp = 
+			new RecurringEventDatePicker(3, DayOfWeek.MONDAY);
+		mp.now = LocalDate.of(2010, 05, 05);
+		@SuppressWarnings("deprecation")
 		Calendar c = mp.getEventDate(0);
 		assertEquals(2010, c.get(Calendar.YEAR));
 		assertEquals(Calendar.MAY, c.get(Calendar.MONTH));
 		assertEquals(17, c.get(Calendar.DAY_OF_MONTH));
-	}
-	
-	@SuppressWarnings("deprecation")
-	private Date makeDate(int y, int m, int d) {
-		return new Date(y-1900, m - 1, d);
 	}
 }
