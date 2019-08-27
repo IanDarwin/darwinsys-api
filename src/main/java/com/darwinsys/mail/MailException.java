11956c7cc24d7c679042cd28fbffac9645e3c38f
Ian Darwin <ian@darwinsys.com>
2019-08-27 15:23:31 -0400

    Fix error in comment

--- src/main/java/com/darwinsys/mail/MailException.java
+++ src/main/java/com/darwinsys/mail/MailException.java
@@ -1,6 +1,6 @@
 package com.darwinsys.mail;
 
-/** Checked Exception for Mail Failures (so rest of code does not
+/** Local Exception for Mail Failures (so rest of code does not
  * need to import javax.mail).
  */
 public class MailException extends RuntimeException {
