2b3a2c23d9a445bd788d648d30cc6d824b729733
Ian Darwin <ian@darwinsys.com>
2019-08-27 11:16:02 -0400

    JPMS module-info; decruft; uncommit .settings/*

--- src/main/java/com/darwinsys/macosui/README.txt
+++
@@ -1,2 +0,0 @@
-The MacOS UI stuff was dead and still dying, so I moved it to its own project,
-macosuiutils. Let me know if you want it or are actually using it.
---
+++ src/main/java/com/darwinsys/module-info.java
@@ -0,0 +1,35 @@
+module com.darwinsys {
+	
+	requires java.desktop;
+	requires java.base;
+    
+	exports com.darwinsys.calendar;
+	exports com.darwinsys.csv;
+	exports com.darwinsys.database;
+	exports com.darwinsys.diff;
+	exports com.darwinsys.formatting;
+	exports com.darwinsys.genericui;
+	exports com.darwinsys.geo;
+	exports com.darwinsys.graphics;
+	exports com.darwinsys.html;
+	exports com.darwinsys.io;
+	exports com.darwinsys.lang;
+	exports com.darwinsys.locks;
+	exports com.darwinsys.mail;
+	exports com.darwinsys.model;
+	exports com.darwinsys.net;
+	exports com.darwinsys.notepad;
+	exports com.darwinsys.preso;
+	exports com.darwinsys.reflection;
+	exports com.darwinsys.regex;
+	exports com.darwinsys.security;
+	exports com.darwinsys.sql;
+	exports com.darwinsys.swingui;
+	exports com.darwinsys.tel;
+	exports com.darwinsys.testdata;
+	exports com.darwinsys.testing;
+	exports com.darwinsys.tools;
+	exports com.darwinsys.unix;
+	exports com.darwinsys.util;
+	exports com.darwinsys.xml;
+}
