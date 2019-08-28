867549ed6c93389271780f69a6ed99d40a628f96
Ian Darwin <ian@darwinsys.com>
2019-08-28 16:29:23 -0400

    Imports police

--- src/test/java/com/darwinsys/testing/TestUtilsTest.java
+++ src/test/java/com/darwinsys/testing/TestUtilsTest.java
@@ -4,15 +4,14 @@ import static com.darwinsys.testing.TestUtils.assertNoDefaultProperties;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
 import org.junit.Before;
-import org.junit.Test;
 import org.junit.Ignore;
+import org.junit.Test;
 
 public class TestUtilsTest {
 
