e17656eb0b9483497ba1a1996363b3abfb2acbb8
Ian Darwin <ian@darwinsys.com>
2021-09-17 09:02:10 -0400

    Imports police

--- src/main/java/com/darwinsys/swingui/FontChooser.java
+++ src/main/java/com/darwinsys/swingui/FontChooser.java
@@ -6,8 +6,6 @@ import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GraphicsEnvironment;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
