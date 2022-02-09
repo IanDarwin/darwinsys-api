4959461ce9f60adf50f37fdcb1288a05997ab878
Ian Darwin <ian@darwinsys.com>
2022-02-09 09:52:15 -0500

    Save this unfinished test

---
+++ src/test/java/com/darwinsys/swingui/RecentMenuTest.java
@@ -0,0 +1,32 @@
+package com.darwinsys.swingui;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RecentMenuTest {
+
+	RecentMenu target;
+
+	@BeforeEach
+	void setup() {
+		target = new RecentMenu(getClass()) {
+			@Override
+			public void loadFile(String fileName) {
+				System.out.println("Test would open " + fileName);
+			}
+		};
+	}
+
+	// @Test
+	void testAddWorks() {
+		target.add(".");
+		assertEquals(1, target.recentFileNames.getList().size());
+	}
+
+	@Test
+	void testClearWorks() {
+		target.add(".");
+		target.clear();
+		assertEquals(0, target.recentFileNames.getList().size());
+	}
+}
