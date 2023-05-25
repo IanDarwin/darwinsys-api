ecce2a0792b06196129c2598b379556750927925
Ian Darwin <ian@darwinsys.com>
2023-05-25 15:51:49 -0400

    Rewrite using Stream class. Emend main() call.

--- src/main/java/com/darwinsys/util/MainStarter.java
+++ src/main/java/com/darwinsys/util/MainStarter.java
@@ -1,10 +1,10 @@
 package com.darwinsys.util;
 
-import java.io.File;
-import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.reflect.Array;
 import java.lang.reflect.Method;
-import java.security.Permission;
-import java.util.Properties;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.function.BiConsumer;
@@ -24,42 +24,54 @@ import javax.swing.JOptionPane;
 @SuppressWarnings("ALL")
 public class MainStarter {
 
-	final static String PROPS_FILE = "mydesktop.properties";
+	final static String PROPS_FILE = ".mydesktop.properties";
 
-	/** The input consists of a class name and ONE optional argument */
-	record ClassAndArg(String clazzName, String arg){}
+	/**
+	 * The input consists of a class name and ONE optional argument.
+	 * If no args, these pairs are read from ~/PROPS_FILE.
+	 * If there are args, each is treated as a class name, with no args.
+	 */
+	private record ClassAndArg(String clazzName, String arg){}
 
 	public static void main(String[] args) throws Exception {
-
-		if (new File(PROPS_FILE).exists()) {
-			Properties props = new Properties();
-			props.load(new FileInputStream(PROPS_FILE));
-			for (Object o : props.keySet()) {
-				String s = (String)o;
-				ClassAndArg caa = new ClassAndArg(s, props.getProperty(s));
-				process(caa);
-			}
+		if (args.length == 0) {
+			processFile(Path.of(System.getProperty("user.home"), PROPS_FILE));
 		} else {
-			if (args.length == 0) {
-				eHandler.accept("Usage: MainStarter propsfile | clazzName [...]", null);
-				System.exit(1);
-			}
 			for (String s : args) {
 				process(new ClassAndArg(s, ""));
 			}
 		}
 	}
 
+	/**
+	 * Process a properties-like file with class name and an arg for each
+	 */
+	static void processFile(Path path) throws IOException {
+		if (!Files.exists(path)) {
+			throw new IllegalArgumentException(path + " " + "Does not exist or not readable");
+		}
+		Files.lines(path).map(s -> s.split("="))
+					.map(a -> new ClassAndArg(a[0], 1==a.length?"":a[1]))
+							.forEach(caa -> process(caa));
+	};
+
+	static final ExecutorService tPool = Executors.newCachedThreadPool();
+
+	/**
+	 * Invoke one ClassAndArg.
+	 * @param caa the ClassAndArg to process
+	 */
 	static void process(ClassAndArg caa) {
-		ExecutorService tPool = Executors.newCachedThreadPool();
 
 			// Catch errors so one mistake doesn't kill the whole thing...
 			try {
 				final Class<?> c = Class.forName(caa.clazzName);
-				final Method m = c.getMethod("main", caa.arg.getClass());
+				final Method m = c.getMethod("main", String[].class);
 				tPool.submit( () -> {
 						try {
-							m.invoke(null, caa.arg);
+							final Object[] args = new Object[1];
+							args[0] = new String[] {caa.arg};
+							m.invoke(null, args);
 						} catch (Exception e) {
 							eHandler.accept(caa.clazzName, e);
 						}
