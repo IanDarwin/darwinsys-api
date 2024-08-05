310914998c23b1e5574a813b7831cc51cbe05e66
Ian Darwin <ian@darwinsys.com>
2024-08-05 17:09:03 -0400

    Finally, a module-info: com.darwinsys.api

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,8 +6,7 @@ module com.darwinsys.api {
 	requires transitive java.sql;
 	requires java.sql.rowset;
 	requires jakarta.mail;
-	requires jakarta.persistence;
-	requires jakarta.xml;
+	requires java.xml;
 
 	exports com.darwinsys.calendar;
 	exports com.darwinsys.csv;
@@ -19,21 +18,23 @@ module com.darwinsys.api {
 	exports com.darwinsys.graphics;
 	exports com.darwinsys.html;
 	exports com.darwinsys.io;
+	opens   com.darwinsys.io;
 	exports com.darwinsys.lang;
 	exports com.darwinsys.locks;
 	provides com.darwinsys.locks.PessimisticLockManager
 		with com.darwinsys.locks.PessimisticLockManagerImpl;
 	exports com.darwinsys.mail;
-	exports com.darwinsys.model;
-	opens com.darwinsys.model;
 	exports com.darwinsys.net;
 	exports com.darwinsys.notepad;
+	exports com.darwinsys.numbers;
+	opens   com.darwinsys.numbers;
 	exports com.darwinsys.preso;
 	exports com.darwinsys.reflection;
 	exports com.darwinsys.regex;
 	exports com.darwinsys.security;
 	exports com.darwinsys.sql;
 	exports com.darwinsys.swingui;
+	opens   com.darwinsys.swingui;
 	exports com.darwinsys.tel;
 	exports com.darwinsys.testdata;
 	exports com.darwinsys.tools;
