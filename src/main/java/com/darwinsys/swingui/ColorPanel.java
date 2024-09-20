0a3a7329d8068adddb042c8a982e4575d82b7a52
Ian Darwin <ian@darwinsys.com>
2024-09-19 21:13:57 -0400

    ColorPanel: macOS special handling for JButton colors

--- src/main/java/com/darwinsys/swingui/ColorPanel.java
+++ src/main/java/com/darwinsys/swingui/ColorPanel.java
@@ -17,9 +17,14 @@ public class ColorPanel extends JPanel {
         this.setter = setter;
         setLayout(new GridLayout(2,4));
         for (Color c : COLORS) {
-            JButton b = new JButton();
+            var b = new JButton();
             b.setPreferredSize(new Dimension(16,16));
+            if (System.getProperty("os.name").toLowerCase().startsWith("mac")) {
+                b.setForeground(c == Color.WHITE ? Color.GRAY : c);
+                b.setText("***");
+            }
             b.setBackground(c);
+            b.setOpaque(true);
             b.addActionListener(evt->setter.accept(c));
             add(b);
         }
@@ -38,4 +43,12 @@ public class ColorPanel extends JPanel {
             setter.accept(ch);
         }
     };
+
+    public static void main(String[] args) {
+        JFrame jf = new JFrame("Dummy");
+        jf.add(new ColorPanel(System.out::println));
+        jf.pack();
+        jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jf.setVisible(true);
+    }
 }
