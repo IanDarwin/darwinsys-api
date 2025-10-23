70f86819b1383575e338d3642bbde6b90e05534e
Ian Darwin <ian@darwinsys.com>
2025-10-23 09:02:22 -0400

    Complex.java moves here. So does Java 25.

---
+++ src/main/java/com/darwinsys/numbers/C.java
@@ -0,0 +1,8 @@
+package com.darwinsys.numbers;
+
+/** Java doesn't allow aliasing of classes but this short form is a must. */
+public class C extends Complex {
+	public C(double r, double i) {
+		super(r, i);
+	}
+}
